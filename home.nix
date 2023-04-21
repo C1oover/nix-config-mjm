6befc34882fdf0b23fcbdb325d7d4fa990398932
Matt Moriarity <matt@slab.com>
2023-04-21 10:12:35 -0600

    install astronvim

--- home.nix
+++ home.nix
@@ -57,6 +57,11 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".config/nvim".source = builtins.fetchGit {
+      url = "https://github.com/AstroNvim/AstroNvim.git";
+      ref = "refs/tags/v3.10.3";
+    };
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
