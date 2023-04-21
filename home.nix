ec447734a1403f084a4b0055c85174768c1aedad
Matt Moriarity <matt@slab.com>
2023-04-21 10:17:29 -0600

    add starship config + some cleanup

--- home.nix
+++ home.nix
@@ -61,36 +61,18 @@
       url = "https://github.com/AstroNvim/AstroNvim.git";
       ref = "refs/tags/v3.10.3";
     };
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/matt/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
+  xdg.configFile = {
+    "starship.toml".source = ./starship.toml;
+  };
+
   home.sessionVariables = {
     SSH_AUTH_SOCK = "/Users/matt/.yubikey-agent.sock";
   };
 
   news.display = "silent";
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.dircolors = {
