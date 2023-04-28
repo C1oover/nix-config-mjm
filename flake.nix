ef9448798be8fe3f54b658cc4e734a6754f67168
Matt Moriarity <matt@mattmoriarity.com>
2023-04-27 21:00:25 -0600

    add nixos config for alecto

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
       nixosConfigurations = {
         megaera = mkNixos [ ./hosts/megaera ];
         tisiphone = mkNixos [ ./hosts/tisiphone ];
+        alecto = mkNixos [ ./hosts/alecto ];
       };
 
       formatter = flake-utils.lib.eachDefaultSystemMap (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
---
+++ hosts/alecto/default.nix
@@ -0,0 +1,22 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../common/global/nixos.nix
+    ../common/users/matt
+
+    ../common/optional/consul-server.nix
+  ];
+
+  networking.hostName = "alecto";
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+  };
+
+  services.qemuGuest.enable = true;
+
+  system.stateVersion = "22.11";
+}
