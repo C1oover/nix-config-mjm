75b462abf9b5db1165aab1d1cea0cb6a049ab31b
Matt Moriarity <matt@mattmoriarity.com>
2023-04-27 18:43:07 -0600

    add checksums to items fetched from vault

--- hosts/common/global/ssh.nix
+++ hosts/common/global/ssh.nix
@@ -1,5 +1,8 @@
 let
-  sshTrustedKeys = builtins.fetchurl "http://vault.service.consul:8200/v1/ssh-client-signer/public_key";
+  sshTrustedKeys = builtins.fetchurl {
+    url = "http://vault.service.consul:8200/v1/ssh-client-signer/public_key";
+    sha256 = "12kcpl2mnfds458fv0c0jb0lz122q9jd7vqcfc7cw27giqis2dgl";
+  };
 in
 {
   services.openssh = {
--- hosts/common/global/ssl.nix
+++ hosts/common/global/ssl.nix
@@ -1,5 +1,8 @@
 let
-  homelabCA = builtins.fetchurl "http://vault.service.consul:8200/v1/pki-homelab/ca/pem";
+  homelabCA = builtins.fetchurl {
+    url = "http://vault.service.consul:8200/v1/pki-homelab/ca/pem";
+    sha256 = "184c68h0kkzkfbw2q80ggpwxvk0gh01bnsv6l7afvbv08s6jhk7c";
+  };
 in
 {
   environment.etc."ssl/homelab.pem".source = homelabCA;
