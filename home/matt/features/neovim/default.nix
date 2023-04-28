eb2768738e334f23a2835ee741b5d9af44272b56
Matt Moriarity <matt@slab.com>
2023-04-28 15:51:30 -0600

    use nix to manage nvim-treesitter and its grammars

--- home/matt/features/neovim/default.nix
+++ home/matt/features/neovim/default.nix
@@ -1,19 +1,43 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  astronvim = builtins.fetchGit {
+    url = "https://github.com/AstroNvim/AstroNvim.git";
+    ref = "refs/tags/v3.11.5";
+    rev = "33b3119d98a9441ff73103cfb705c33122afb632";
+    shallow = true;
+  };
+in
+{
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
     defaultEditor = true;
+
+    plugins = with pkgs.vimPlugins; [
+      (nvim-treesitter.withPlugins (p: [
+        p.bash
+        p.dockerfile
+        p.eex
+        p.elixir
+        p.gitcommit
+        p.gitignore
+        p.graphql
+        p.heex
+        p.json
+        p.lua
+        p.markdown
+        p.nix
+        p.proto
+        p.starlark
+        p.toml
+        p.yaml
+      ]))
+    ];
   };
 
-  xdg.configFile."nvim".source = builtins.fetchGit {
-    url = "https://github.com/AstroNvim/AstroNvim.git";
-    ref = "refs/tags/v3.11.5";
-    rev = "33b3119d98a9441ff73103cfb705c33122afb632";
-    shallow = true;
-  };
-
+  xdg.configFile."nvim".source = astronvim;
   xdg.configFile."astronvim/lua/user".source = ./user;
 
   home.packages = with pkgs; [
--- home/matt/features/neovim/user/init.lua
+++ home/matt/features/neovim/user/init.lua
@@ -7,4 +7,17 @@ return {
       "rnix",
     },
   },
+  lazy = {
+    performance = {
+      reset_packpath = false,
+      rtp = {
+        reset = false,
+      },
+    },
+  },
+  polish = function()
+    -- lazy.nvim disables the built-in package management,
+    -- so we need to run it manually here to get plugins from Nix
+    vim.cmd([[packloadall]])
+  end,
 }
