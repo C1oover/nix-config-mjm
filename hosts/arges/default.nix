b0e29e6935cb436fc1b3621b5045c7f15dd346c5
Matt Moriarity <matt@mattmoriarity.com>
2023-04-29 23:21:14 -0600

    lol it changed back

--- hosts/arges/default.nix
+++ hosts/arges/default.nix
@@ -16,8 +16,7 @@
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  # no, I don't know why the interface name is different from brontes.
-  services.consul.interface.advertise = "eth0";
+  services.consul.interface.advertise = "end0";
 
   system.stateVersion = "21.03";
 }
