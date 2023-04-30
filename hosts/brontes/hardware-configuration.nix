14d6d2fa6d06d42e36c19736c06892f23287a540
Matt Moriarity <matt@mattmoriarity.com>
2023-04-30 07:46:57 -0600

    add hardware modules to arges and brontes

--- hosts/brontes/hardware-configuration.nix
+++ hosts/brontes/hardware-configuration.nix
@@ -1,23 +1,7 @@
-{ lib
-, pkgs
-, ...
-}: {
-  boot.kernelPackages = pkgs.linuxPackages_rpi4;
-  boot.initrd.availableKernelModules = [ "usbhid" ];
+{ lib, ... }: {
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-
-  # without this config setting, envoy can't run
-  # boot.kernelPatches = [
-  #   {
-  #     name = "pgtables";
-  #     patch = null;
-  #     extraConfig = ''
-  #       PGTABLE_LEVELS 4
-  #     '';
-  #   }
-  # ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
@@ -26,6 +10,8 @@
   };
 
   swapDevices = [ ];
+
+  hardware.raspberry-pi."4".poe-hat.enable = true;
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
