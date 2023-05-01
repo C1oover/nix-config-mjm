c4439d59956d124e7dadf5c788a20cd2750d44f5
Matt Moriarity <matt@mattmoriarity.com>
2023-04-30 19:45:15 -0600

    vault: enable metrics

--- hosts/common/optional/vault-server.nix
+++ hosts/common/optional/vault-server.nix
@@ -1,13 +1,20 @@
-{ pkgs, ... }:
-
-{
+{ pkgs, ... }: {
   services.vault = {
     enable = true;
     package = pkgs.vault-bin;
     address = "0.0.0.0:8200";
     storageBackend = "consul";
+    listenerExtraConfig = ''
+      telemetry {
+        unauthenticated_metrics_access = true
+      }
+    '';
     extraConfig = ''
       ui = true
+
+      telemetry {
+        disable_hostname = true
+      }
     '';
   };
 
