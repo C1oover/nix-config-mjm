d4eee7495e9efab8065a94a5a9a061fbb1737b3d
Matt Moriarity <matt@mattmoriarity.com>
2023-04-30 19:59:54 -0600

    nomad: enable prometheus metrics

--- hosts/common/optional/nomad-server.nix
+++ hosts/common/optional/nomad-server.nix
@@ -11,6 +11,13 @@
         address = "http://127.0.0.1:8200";
         create_from_role = "nomad-cluster";
       };
+      telemetry = {
+        collection_interval = "1s";
+        disable_hostname = true;
+        prometheus_metrics = true;
+        publish_allocation_metrics = true;
+        publish_node_metrics = true;
+      };
     };
     credentials = {
       "nomad-vault-config.json" = config.age.secrets."nomad-vault-config.json".path;
