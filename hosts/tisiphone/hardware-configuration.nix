766a19387698c06880a7729bbcc9a4022b538d8e
Matt Moriarity <matt@mattmoriarity.com>
2023-05-02 17:46:17 -0600

    remove unused params

--- hosts/tisiphone/hardware-configuration.nix
+++ hosts/tisiphone/hardware-configuration.nix
@@ -1,19 +1,21 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
-{
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+{ config
+, lib
+, modulesPath
+, ...
+}: {
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
 
   swapDevices = [ ];
 
