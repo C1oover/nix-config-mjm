331ee8e1cd5b3d95289f69040832b86d4f8c21e2
Matt Moriarity <matt@mattmoriarity.com>
2023-05-01 18:17:19 -0600

    vault: fix raft for real?

--- hosts/common/optional/vault-server-raft.nix
+++ hosts/common/optional/vault-server-raft.nix
@@ -17,8 +17,8 @@
       }
     '';
     extraConfig = ''
-      api_addr = "http://{{ GetInterfaceIP \\"ens18\\" }}:8200"
-      cluster_addr = "https://{{ GetInterfaceIP \\"ens18\\" }}:8201"
+      api_addr = "http://{{ GetInterfaceIP \"ens18\" }}:8200"
+      cluster_addr = "https://{{ GetInterfaceIP \"ens18\" }}:8201"
       disable_mlock = true
       ui = true
 
