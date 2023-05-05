504fde4ee60459dd1f2a368f9f6add10085427b5
Matt Moriarity <matt@slab.com>
2023-05-05 13:19:30 -0600

    neovim: remember last test ran and allow running it again

--- home/matt/features/neovim/user/mappings.lua
+++ home/matt/features/neovim/user/mappings.lua
@@ -1,3 +1,13 @@
+local run_last_test = function()
+	os.execute(
+		"kitty @ --to="
+		.. os.getenv("KITTY_LISTEN_ON")
+		.. " launch --type=overlay --cwd=current zsh -l -i -c 'docker compose exec slab_1 mix "
+		.. vim.g.my_last_test
+		.. "; read'"
+	)
+end
+
 return {
 	n = {
 		["<leader>kf"] = {
@@ -6,39 +16,25 @@ return {
 			end,
 			desc = "Copy relative path",
 		},
+		["<leader>TT"] = { run_last_test, desc = "Re-run last test" },
 		["<leader>TA"] = {
 			function()
-				os.execute(
-					"kitty @ --to="
-					.. os.getenv("KITTY_LISTEN_ON")
-					.. " launch --type=overlay --cwd=current zsh -l -i -c 'docker compose exec slab_1 mix test.all; read'"
-				)
+				vim.g.my_last_test = "test.all"
+				run_last_test()
 			end,
 			desc = "Test entire project",
 		},
 		["<leader>TF"] = {
 			function()
-				os.execute(
-					"kitty @ --to="
-					.. os.getenv("KITTY_LISTEN_ON")
-					.. " launch --type=overlay --cwd=current zsh -l -i -c 'docker compose exec slab_1 mix test "
-					.. vim.fn.expand("%")
-					.. "; read'"
-				)
+				vim.g.my_last_test = "test " .. vim.fn.expand("%")
+				run_last_test()
 			end,
 			desc = "Test current file",
 		},
 		["<leader>TL"] = {
 			function()
-				os.execute(
-					"kitty @ --to="
-					.. os.getenv("KITTY_LISTEN_ON")
-					.. " launch --type=overlay --cwd=current zsh -l -i -c 'docker compose exec slab_1 mix test "
-					.. vim.fn.expand("%")
-					.. ":"
-					.. vim.api.nvim_win_get_cursor(0)[1]
-					.. "; read'"
-				)
+				vim.g.my_last_test = "test " .. vim.fn.expand("%") .. ":" .. vim.api.nvim_win_get_cursor(0)[1]
+				run_last_test()
 			end,
 			desc = "Test current line",
 		},
