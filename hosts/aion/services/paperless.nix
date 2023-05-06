9293337b04a6a493184bb388a8ef8e06aefe3a3b
Matt Moriarity <matt@mattmoriarity.com>
2023-05-05 22:49:21 -0600

    paperless: fixes to get everything running

--- hosts/aion/services/paperless.nix
+++ hosts/aion/services/paperless.nix
@@ -4,6 +4,7 @@
 }:
 let
   format = pkgs.formats.json { };
+  scannerPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVffhmmioPFJxQiP5OlssYk2EjHdeMxpV1OO2T3Qz3AjmMZsJunQdnpWV9wNEeG3uTwGmvDS4ejCoJvVy6kQMypFFjEqBegbK6N4HeMFYubxe2pp2NSZir1HEeHFYZSvrIjOKfN404WLpY/+TgM7UTQ5u5pUNmMPyyxLgZz/YJj9LVCo1IeYcv5hP3WSP1ixsJGBuUTgkVp4S/FouoHNoJ+jVbGSs0IjgBC77IzxH52Af/sXwb2MiRoM3HczEwnWuhiIniICCbjgju5hA0h1qCozRDx5jt6egKxzNxN7+tuW3S9t1D/sq/qn6hqyaTCHVxn9+2cw3KIXcK7LoduhX5 root@BR5CF370B3F03D";
 in
 {
   services.paperless = {
@@ -43,7 +44,7 @@ in
   systemd.services.paperless-vault-agent =
     let
       roleId = pkgs.writeText "paperless-role-id" "11a736d8-ef30-f7aa-1d1e-72029ce45fb4";
-      restartScript = pkgs.writeShellScriptBin "paperless-restart" ''
+      restartScript = pkgs.writeShellScript "paperless-restart" ''
         set -e
 
         systemctl restart paperless-scheduler.service
@@ -79,10 +80,7 @@ in
       };
     in
     {
-      # leave this turned off to avoid creating buttloads of users
-      enable = false;
-
-      description = "Vault agent to provide rotating database credentials for Paperless";
+      description = "Vault Agent for Paperless";
 
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
@@ -103,13 +101,53 @@ in
       };
     };
 
+  # Fix service configs so things actually are able to run.
+  # Not sure if this is needed because this is running inside a container or what.
   systemd.services.paperless-scheduler.serviceConfig.EnvironmentFile = "/run/secrets/paperless/paperless.env";
+  systemd.services.paperless-scheduler.serviceConfig.ProtectHostname = lib.mkForce false;
+  systemd.services.paperless-scheduler.serviceConfig.PrivateUsers = lib.mkForce false;
+  systemd.services.paperless-scheduler.serviceConfig.PrivateNetwork = lib.mkForce false;
   systemd.services.paperless-task-queue.serviceConfig.EnvironmentFile = "/run/secrets/paperless/paperless.env";
+  systemd.services.paperless-task-queue.serviceConfig.ProtectHostname = lib.mkForce false;
+  systemd.services.paperless-task-queue.serviceConfig.PrivateUsers = lib.mkForce false;
   systemd.services.paperless-consumer.serviceConfig.EnvironmentFile = "/run/secrets/paperless/paperless.env";
+  systemd.services.paperless-consumer.serviceConfig.PrivateNetwork = lib.mkForce false;
+  systemd.services.paperless-consumer.serviceConfig.ProtectHostname = lib.mkForce false;
+  systemd.services.paperless-consumer.serviceConfig.PrivateUsers = lib.mkForce false;
   systemd.services.paperless-web.serviceConfig.EnvironmentFile = "/run/secrets/paperless/paperless.env";
-  systemd.services.paperless-download-nltk-data.serviceConfig.ProtectHostname = false;
+  systemd.services.paperless-web.serviceConfig.ProtectHostname = lib.mkForce false;
+  systemd.services.paperless-web.serviceConfig.PrivateUsers = lib.mkForce false;
+  systemd.services.paperless-download-nltk-data.serviceConfig.ProtectHostname = lib.mkForce false;
+  systemd.services.paperless-download-nltk-data.serviceConfig.PrivateUsers = lib.mkForce false;
 
-  # TODO: sshd config to accept uploads from scanner
+  users.users.paperless.openssh.authorizedKeys.keys = [ scannerPublicKey ];
+
+  services.openssh.settings.KexAlgorithms = [
+    "sntrup761x25519-sha512@openssh.com"
+    "curve25519-sha256"
+    "curve25519-sha256@libssh.org"
+    "diffie-hellman-group-exchange-sha256"
+    # this is the extra one added for compatibility with old SSH client
+    "diffie-hellman-group14-sha1"
+  ];
+  services.openssh.settings.Macs = [
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+    "hmac-sha2-512"
+    "hmac-sha2-256"
+    "umac-128@openssh.com"
+    # this is the extra one added for compatibility with old SSH client
+    "hmac-sha1-96"
+  ];
+  services.openssh.extraConfig = ''
+    HostKeyAlgorithms +ssh-rsa
+    PubkeyAcceptedAlgorithms +ssh-rsa
+    Match User paperless
+        X11Forwarding no
+        AllowTcpForwarding no
+        ForceCommand internal-sftp -u 0077 -d /var/lib/paperless/consume
+  '';
 
   age.secrets."paperless-approle-secret-id" = {
     file = ../../../secrets/paperless-approle-secret-id.age;
