d792e1f2af9c9c686c20b10c81e45fb2d8960d76
Matt Moriarity <matt@slab.com>
2023-05-10 09:55:59 -0600

    nomad: add keys and secret config for docker auth

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,15 +6,23 @@ let
   megaera = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjL++8eZk0Ydrti+7EcyEvZp5AHCKz+R8IQ9ohs/KT7";
   tisiphone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqaFj8PGrEhie1P0uEKJ6lbP0fjB+CzuWw5siQGlsin";
   alecto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINq0z6ClXVPLPyucJEPg5EhqK//2JVexFGO4d9MT+cJY";
+  arges = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDS3pPNEaHA+ycDa7kTyNSxsAYBFZI7YMweDrrN0Gg+l";
+  brontes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDK3Cibu7YdRQeQcTWKOB59ExP+UsaJbW5F+lrvEbBUJ";
+  steropes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAjKfsGgQgS1q/t73Y/kFh9fPZwlPH2iyZDqW2rKC2uL";
   hypnos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKn/ufUVxaDdTElgs61xfvsHsHn3RwpL7n6DO5qcBO0K";
+  helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHUqz5VSSneEjQXfTrFiK8dfbclhClA9faM1gobRaqJy";
   orion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFDLe0Oy6quuAu4ARXnZCWNDiYTgfdLjy7kp7MB8n4G";
   nemesis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOBXAwxTy5mDt9jxKg1rhRryyNr5jkIPHnwAdLsQ1yI";
   aion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKFrAsGVNyUfMfNvdix1ON28I/eu4MBhU6YdzzgRItQf";
+
+  nomadClients = [ arges brontes steropes helios ];
 in
 {
   "megaera-nomad-vault-config.age".publicKeys = [ matt-mars megaera ];
   "tisiphone-nomad-vault-config.age".publicKeys = [ matt-mars tisiphone ];
   "alecto-nomad-vault-config.age".publicKeys = [ matt-mars alecto ];
+
+  "nomad-docker-auth.age".publicKeys = [ matt-mars ] ++ nomadClients;
 
   "gitlab-runner-registration.age".publicKeys = [ matt-mars hypnos ];
 
