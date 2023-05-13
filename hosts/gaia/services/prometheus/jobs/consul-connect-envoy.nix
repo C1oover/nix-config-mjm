8ec44ea7e0237e50dcfe2fe9df13fcd35f137bff
Matt Moriarity <matt@mattmoriarity.com>
2023-05-13 10:49:46 -0600

    prometheus: fix accidental interpolations

--- hosts/gaia/services/prometheus/jobs/consul-connect-envoy.nix
+++ hosts/gaia/services/prometheus/jobs/consul-connect-envoy.nix
@@ -30,7 +30,7 @@
         {
           source_labels = [ "__address__" "__meta_consul_service_metadata_envoy_metrics_port" ];
           regex = "([^:]+)(?::\\d+)?;(\\d+)";
-          replacement = "${1}:${2}";
+          replacement = "$1:$2";
           target_label = "__address__";
         }
       ];
