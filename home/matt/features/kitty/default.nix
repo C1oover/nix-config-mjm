def4a364fdbd488d66d8914d48ebc058215a39fa
Matt Moriarity <matt@mattmoriarity.com>
2023-05-13 20:50:20 -0600

    kitty: set default layouts

--- home/matt/features/kitty/default.nix
+++ home/matt/features/kitty/default.nix
@@ -10,6 +10,7 @@
       tab_bar_style = "powerline";
       macos_option_as_alt = "both";
       allow_remote_control = "yes";
+      enabled_layouts = "tall:bias=65;full_size=1,fat:bias=70;full_size=1,stack";
     };
     keybindings = {
       "cmd+enter" = "launch --cwd=current";
