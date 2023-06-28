a910bb0d4a734c84ab6e252045ab68072ede5133
Matt Moriarity <matt@mattmoriarity.com>
2023-06-27 23:13:22 -0600

    gh: install on all darwin machines

--- home/matt/athena.nix
+++ home/matt/athena.nix
@@ -11,7 +11,6 @@
   ];
 
   home.packages = with pkgs; [
-    gh
     google-cloud-sdk
     ngrok
     teams
--- home/matt/global/default.nix
+++ home/matt/global/default.nix
@@ -28,6 +28,7 @@
 
   home.packages = with pkgs; [
     colima
+    gh
     httpie
     pstree
     ripgrep
