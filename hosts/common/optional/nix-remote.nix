f7cd4772527d7e5eefdfd56801b268637f06de65
Matt Moriarity <matt@mattmoriarity.com>
2023-06-30 21:07:35 -0600

    nix-remote: use programs.ssh.extraConfig

--- hosts/common/optional/nix-remote.nix
+++ hosts/common/optional/nix-remote.nix
@@ -1,19 +1,10 @@
-{
-  pkgs,
-  config,
-  ...
-}: let
-  sshConfig = pkgs.writeText "root-ssh-config" ''
+{config, ...}: {
+  programs.ssh.extraConfig = ''
     Host hypnos
         # Prevent using ssh-agent or another keyfile, useful for testing
         IdentitiesOnly yes
         IdentityFile ${config.age.secrets.id_nixremote.path}
         User nixremote
-  '';
-in {
-  system.activationScripts.nixremote-ssh-config.text = ''
-    mkdir -p /root/.ssh
-    ln -sf ${sshConfig} /root/.ssh/config
   '';
 
   nix.distributedBuilds = true;
