d10917a82df5b3ac7e693427f15892d7bcd5babe
Matt Moriarity <matt@mattmoriarity.com>
2023-06-30 20:44:08 -0600

    nix-remote: add speedfactor for remote builder

--- hosts/common/optional/nix-remote.nix
+++ hosts/common/optional/nix-remote.nix
@@ -24,6 +24,7 @@ in {
       system = "x86_64-linux";
       protocol = "ssh-ng";
       maxJobs = 4;
+      speedFactor = 2;
       supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
       mandatoryFeatures = [];
       publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUtuL3VmVVZ4YURkVEVsZ3M2MXhmdnNIc0huM1J3cEw3bjZETzVxY0JPMEsgcm9vdEBoeXBub3MK";
