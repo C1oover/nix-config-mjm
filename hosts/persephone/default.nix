80f4ef5fbe44fa7e3530bda1fdcc9ecffcb5197d
Matt Moriarity <matt@mattmoriarity.com>
2023-07-11 10:17:03 -0600

    Revert "greetd: set up gtkgreet"
    This reverts commit 36cbee1b0d2465266090a8170fba0c2913089d9a.

--- hosts/persephone/default.nix
+++ hosts/persephone/default.nix
@@ -52,23 +52,6 @@
     "ssh/ssh_host_ed25519_key.pub".source = "/persist/etc/ssh/ssh_host_ed25519_key.pub";
     "ssh/ssh_host_rsa_key".source = "/persist/etc/ssh/ssh_host_rsa_key";
     "ssh/ssh_host_rsa_key.pub".source = "/persist/etc/ssh/ssh_host_rsa_key.pub";
-
-    "greetd/environments".text = ''
-      sway
-      zsh
-    '';
-    "greetd/sway-config".text = ''
-      # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-      exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-
-      bindsym Mod4+shift+e exec swaynag \
-        -t warning \
-        -m 'What do you want to do?' \
-        -b 'Poweroff' 'systemctl poweroff' \
-        -b 'Reboot' 'systemctl reboot'
-
-      include /etc/sway/config.d/*
-    '';
   };
 
   security.sudo.extraConfig = ''
@@ -110,7 +93,8 @@
     enable = true;
     settings = {
       default_session = {
-        command = "sway --config /etc/greetd/sway-config";
+        command = "sway";
+        user = "matt";
       };
     };
   };
