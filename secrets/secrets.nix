ceff09c6f72546a1276c78e71b7169ee7f82d9d3
Matt Moriarity <matt@mattmoriarity.com>
2023-07-13 21:12:03 -0600

    agenix: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -35,36 +35,37 @@ let
     gaia
     persephone
   ];
+  personalKeys = [matt-mars matt-persephone];
 in {
-  "megaera-nomad-vault-config.age".publicKeys = [matt-mars megaera];
-  "tisiphone-nomad-vault-config.age".publicKeys = [matt-mars tisiphone];
-  "alecto-nomad-vault-config.age".publicKeys = [matt-mars alecto];
+  "megaera-nomad-vault-config.age".publicKeys = personalKeys ++ [megaera];
+  "tisiphone-nomad-vault-config.age".publicKeys = personalKeys ++ [tisiphone];
+  "alecto-nomad-vault-config.age".publicKeys = personalKeys ++ [alecto];
 
-  "nomad-docker-auth.age".publicKeys = [matt-mars] ++ nomadClients;
+  "nomad-docker-auth.age".publicKeys = personalKeys ++ nomadClients;
 
-  "gitlab-runner-registration.age".publicKeys = [matt-mars hypnos];
+  "gitlab-runner-registration.age".publicKeys = personalKeys ++ [hypnos];
 
-  "authelia-jwt-secret.age".publicKeys = [matt-mars orion];
-  "authelia-jwt-private-key.age".publicKeys = [matt-mars orion];
-  "authelia-storage-encryption-key.age".publicKeys = [matt-mars orion];
-  "authelia-session-secret.age".publicKeys = [matt-mars orion];
-  "authelia-hmac-secret.age".publicKeys = [matt-mars orion];
-  "authelia-smtp-password.age".publicKeys = [matt-mars orion];
-  "authelia-ldap-password.age".publicKeys = [matt-mars orion];
-  "authelia-approle-secret-id.age".publicKeys = [matt-mars orion];
+  "authelia-jwt-secret.age".publicKeys = personalKeys ++ [orion];
+  "authelia-jwt-private-key.age".publicKeys = personalKeys ++ [orion];
+  "authelia-storage-encryption-key.age".publicKeys = personalKeys ++ [orion];
+  "authelia-session-secret.age".publicKeys = personalKeys ++ [orion];
+  "authelia-hmac-secret.age".publicKeys = personalKeys ++ [orion];
+  "authelia-smtp-password.age".publicKeys = personalKeys ++ [orion];
+  "authelia-ldap-password.age".publicKeys = personalKeys ++ [orion];
+  "authelia-approle-secret-id.age".publicKeys = personalKeys ++ [orion];
 
-  "netbox-secret-key.age".publicKeys = [matt-mars nemesis];
-  "netbox-approle-secret-id.age".publicKeys = [matt-mars nemesis];
+  "netbox-secret-key.age".publicKeys = personalKeys ++ [nemesis];
+  "netbox-approle-secret-id.age".publicKeys = personalKeys ++ [nemesis];
 
-  "paperless-approle-secret-id.age".publicKeys = [matt-mars aion];
+  "paperless-approle-secret-id.age".publicKeys = personalKeys ++ [aion];
 
-  "alertmanager-env.age".publicKeys = [matt-mars gaia];
+  "alertmanager-env.age".publicKeys = personalKeys ++ [gaia];
 
   "ngrok.age".publicKeys = [matt-athena athena];
 
-  "newsboat-miniflux-token.age".publicKeys = [matt-mars matt-athena];
+  "newsboat-miniflux-token.age".publicKeys = personalKeys;
 
-  "nixremote-key.age".publicKeys = [matt-mars] ++ allNixOS;
+  "nixremote-key.age".publicKeys = personalKeys ++ allNixOS;
 
-  "wpa-supplicant-env.age".publicKeys = [matt-mars matt-persephone persephone];
+  "wpa-supplicant-env.age".publicKeys = personalKeys ++ [persephone];
 }
