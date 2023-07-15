6bc30dbc9685b7714c5ade16f55748d9e8610d75
Matt Moriarity <matt@mattmoriarity.com>
2023-07-15 12:44:03 -0600

    minio: add env vars

--- hosts/phoebe/services/minio.nix
+++ hosts/phoebe/services/minio.nix
@@ -3,6 +3,13 @@
     enable = true;
   };
 
+  systemd.services.minio.environment = {
+    MINIO_PROMETHEUS_AUTH_TYPE = "public";
+    MINIO_PROMETHEUS_URL = "http://prometheus.service.consul:9090";
+    MINIO_PROMETHEUS_JOB_ID = "consul-services";
+    MINIO_BROWSER_REDIRECT_URL = "https://minio-console.home.mattmoriarity.com";
+  };
+
   networking.firewall.allowedTCPPorts = [
     9000
     9001
