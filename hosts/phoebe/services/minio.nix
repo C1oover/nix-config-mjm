22d7ce46258b29d703063758ce060ded674c6460
Matt Moriarity <matt@mattmoriarity.com>
2023-07-15 12:38:15 -0600

    minio: enable consul service

--- hosts/phoebe/services/minio.nix
+++ hosts/phoebe/services/minio.nix
@@ -8,32 +8,32 @@
     9001
   ];
 
-  # services.consul.services = {
-  #   minio = {
-  #     port = 9000;
-  #     meta.metrics_path = "/minio/v2/metrics/cluster";
-  #
-  #     checks = [
-  #       {
-  #         name = "minio is ready";
-  #         http = "http://localhost:9000/minio/health/cluster";
-  #         interval = "15s";
-  #         timeout = "10s";
-  #       }
-  #     ];
-  #   };
-  #
-  #   minio-console = {
-  #     port = 9001;
-  #
-  #     checks = [
-  #       {
-  #         name = "minio-console is ready";
-  #         http = "http://localhost:9001/";
-  #         interval = "15s";
-  #         timeout = "10s";
-  #       }
-  #     ];
-  #   };
-  # };
+  services.consul.services = {
+    minio = {
+      port = 9000;
+      meta.metrics_path = "/minio/v2/metrics/cluster";
+
+      checks = [
+        {
+          name = "minio is ready";
+          http = "http://localhost:9000/minio/health/cluster";
+          interval = "15s";
+          timeout = "10s";
+        }
+      ];
+    };
+
+    minio-console = {
+      port = 9001;
+
+      checks = [
+        {
+          name = "minio-console is ready";
+          http = "http://localhost:9001/";
+          interval = "15s";
+          timeout = "10s";
+        }
+      ];
+    };
+  };
 }
