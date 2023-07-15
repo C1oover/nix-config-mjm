860aed9e35af6e2efea5aadcaf8e18031f0b729e
Matt Moriarity <matt@mattmoriarity.com>
2023-07-15 10:55:05 -0600

    phoebe: enable minio

--- hosts/phoebe/default.nix
+++ hosts/phoebe/default.nix
@@ -11,6 +11,8 @@
     ../common/optional/server
     ../common/optional/consul-agent.nix
     ../common/optional/nix-remote.nix
+
+    ./services/minio.nix
   ];
 
   networking.hostName = "phoebe";
