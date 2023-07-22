4a43eeda72aac9b47418fbdb68187c0199621a39
Matt Moriarity <matt@mattmoriarity.com>
2023-07-22 13:05:00 -0600

    nyx: set proxy headers

--- hosts/nyx/services/nginx.nix
+++ hosts/nyx/services/nginx.nix
@@ -14,7 +14,16 @@
       "auth.mattmoriarity.com" = {
         forceSSL = true;
         enableACME = true;
-        locations."/".proxyPass = "http://ingress";
+        locations."/" = {
+          proxyPass = "http://ingress";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Host $http_host;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+        };
       };
     };
   };
