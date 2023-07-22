75ed9963f6a2f626658010772dc4e9f13921f6d9
Matt Moriarity <matt@mattmoriarity.com>
2023-07-22 12:11:05 -0600

    orion: set up additional authelia instance

--- hosts/orion/services/authelia.nix
+++ hosts/orion/services/authelia.nix
@@ -56,8 +56,7 @@
     }
   ];
 
-  user = config.services.authelia.instances.main.user;
-  group = config.services.authelia.instances.main.group;
+  inherit (config.services.authelia.instances.main) user group;
 in {
   services.authelia.instances.main = {
     enable = true;
@@ -140,6 +139,74 @@ in {
     ];
   };
 
+  services.authelia.instances.external = {
+    inherit user group;
+    enable = true;
+    settings = {
+      default_redirection_url = "https://auth.mattmoriarity.com/";
+      default_2fa_method = "webauthn";
+      server.host = "0.0.0.0";
+      server.port = 9092;
+      telemetry.metrics = {
+        enabled = true;
+        address = "tcp://0.0.0.0:9960";
+      };
+      webauthn.display_name = "Homelab External";
+      authentication_backend.ldap = {
+        implementation = "custom";
+        url = "ldap://localhost:3890";
+        timeout = "5s";
+        start_tls = false;
+        base_dn = "dc=home,dc=mattmoriarity,dc=com";
+        username_attribute = "uid";
+        additional_users_dn = "ou=people";
+        users_filter = "(&({username_attribute}={input})(objectClass=person))";
+        additional_groups_dn = "ou=groups";
+        groups_filter = "(member={dn})";
+        group_name_attribute = "cn";
+        mail_attribute = "mail";
+        display_name_attribute = "displayName";
+        user = "uid=service,ou=people,dc=home,dc=mattmoriarity,dc=com";
+      };
+      access_control = {
+        default_policy = "two_factor";
+        rules = [];
+      };
+      session.domain = "mattmoriarity.com";
+      session.redis = {
+        host = "redis.service.consul";
+        port = 6379;
+        database_index = 3;
+      };
+      storage.postgres = {
+        host = "postgresql.service.consul";
+        port = 5432;
+        database = "authelia_external";
+      };
+      notifier.smtp = {
+        host = "smtp.fastmail.com";
+        port = 587;
+        username = "matt@mattmoriarity.com";
+        sender = "Authelia <admin@mattmoriarity.com>";
+      };
+      identity_providers.oidc.clients = oidcClients;
+    };
+    secrets = {
+      jwtSecretFile = config.age.secrets."authelia-jwt-secret".path;
+      oidcHmacSecretFile = config.age.secrets."authelia-hmac-secret".path;
+      oidcIssuerPrivateKeyFile = config.age.secrets."authelia-jwt-private-key".path;
+      sessionSecretFile = config.age.secrets."authelia-session-secret".path;
+      storageEncryptionKeyFile = config.age.secrets."authelia-storage-encryption-key".path;
+    };
+    environmentVariables = {
+      AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE = config.age.secrets."authelia-ldap-password".path;
+      AUTHELIA_NOTIFIER_SMTP_PASSWORD_FILE = config.age.secrets."authelia-smtp-password".path;
+    };
+    settingsFiles = [
+      "/run/secrets/authelia/db-config.yml"
+    ];
+  };
+
   networking.firewall.allowedTCPPorts = [
     9091
     9959
@@ -157,6 +224,24 @@ in {
       {
         name = "authelia is ready";
         http = "http://localhost:9091/api/health";
+        interval = "30s";
+        timeout = "5s";
+      }
+    ];
+  };
+
+  services.consul.services.authelia-external = {
+    port = 9092;
+
+    meta = {
+      metrics_path = "/metrics";
+      metrics_port = "9960";
+    };
+
+    checks = [
+      {
+        name = "authelia is ready";
+        http = "http://localhost:9092/api/health";
         interval = "30s";
         timeout = "5s";
       }
@@ -182,7 +267,7 @@ in {
             {{ end }}
           '';
           destination = "/run/secrets/authelia/db-config.yml";
-          command = "systemctl restart authelia-main.service";
+          command = "systemctl restart authelia-main.service authelia-external.service";
         }
       ];
     };
