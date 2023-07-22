0814cdf43ffd2e77b0e751a83bb2655790b87dd6
Matt Moriarity <matt@mattmoriarity.com>
2023-07-22 13:12:09 -0600

    nyx: comment some of the headers

--- hosts/nyx/services/nginx.nix
+++ hosts/nyx/services/nginx.nix
@@ -18,10 +18,10 @@
           proxyPass = "http://ingress";
           extraConfig = ''
             proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Proto $scheme;
+            # proxy_set_header X-Real-IP $remote_addr;
+            # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            # proxy_set_header X-Forwarded-Host $http_host;
+            # proxy_set_header X-Forwarded-Proto $scheme;
           '';
         };
       };
