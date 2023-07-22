608615c0c82b12693b797b978c76bd773ae934c0
Matt Moriarity <matt@mattmoriarity.com>
2023-07-22 12:52:14 -0600

    nyx: set up certs and auth proxy

--- hosts/nyx/services/nginx.nix
+++ hosts/nyx/services/nginx.nix
@@ -1,6 +1,21 @@
 {
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "acme@matt.mattmoriarity.com";
+
   services.nginx = {
     enable = true;
+    upstreams.ingress.servers = {
+      # hardcoded for now, will need to be templated from consul
+      "100.113.14.91" = {};
+      "100.103.187.51" = {};
+    };
+    virtualHosts = {
+      "auth.mattmoriarity.com" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://ingress";
+      };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [80 443];
