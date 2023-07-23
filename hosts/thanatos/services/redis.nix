4c75818a4f234c7c112a93ac8963619c96afd261
Matt Moriarity <matt@mattmoriarity.com>
2023-07-22 21:59:10 -0600

    thanatos: enable redis

---
+++ hosts/thanatos/services/redis.nix
@@ -0,0 +1,11 @@
+{config, ...}: {
+  services.redis.servers."" = {
+    enable = true;
+    openFirewall = true;
+    bind = null;
+  };
+
+  # services.consul.services.redis = {
+  #   inherit (config.services.redis.servers."") port;
+  # };
+}
