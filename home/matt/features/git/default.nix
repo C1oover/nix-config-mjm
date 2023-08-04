d2f217c4134e558c07235d994925ab370327742b
Matt Moriarity <matt@mattmoriarity.com>
2023-08-04 08:03:50 -0600

    git: remove jujutsu zsh completion workaround
    my fix for this was merged into home-manager

--- home/matt/features/git/default.nix
+++ home/matt/features/git/default.nix
@@ -62,16 +62,7 @@
       user.email = config.programs.git.userEmail;
       ui.default-command = "log";
     };
-    enableZshIntegration = false;
   };
-
-  # the zsh integration in home-manager chops off the last line, which closes an if statement.
-  # perhaps there used to be an erroneous line in the output that needed to be removed, but there isn't now.
-  # so we'll just do this ourselves for the time being.
-  programs.zsh.initExtra = ''
-    source <(${pkgs.jujutsu}/bin/jj util completion --zsh)
-    compdef _jj ${pkgs.jujutsu}/bin/jj
-  '';
 
   home.shellAliases = {
     jj-push = "jj branch set main -r @- && jj git push";
