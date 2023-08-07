cde93be4369655a4038abf4eaf61ccafa3246480
Matt Moriarity <matt@mattmoriarity.com>
2023-08-07 17:05:37 -0600

    persephone: add daily snapshots of home and persist

--- hosts/persephone/default.nix
+++ hosts/persephone/default.nix
@@ -9,6 +9,7 @@
     inputs.hardware.nixosModules.common-pc-laptop-ssd
     ./hardware-configuration.nix
     ./impermanence.nix
+    ./snapshots.nix
     ./virtualization.nix
 
     ../common/global/nixos
---
+++ hosts/persephone/snapshots.nix
@@ -0,0 +1,39 @@
+{
+  services.btrbk.instances.btrbk = {
+    onCalendar = "daily";
+    settings = {
+      snapshot_preserve_min = "2d";
+      snapshot_preserve = "14d";
+      volume."/mnt" = {
+        snapshot_dir = "snapshots";
+        subvolume."home" = {};
+        subvolume."persist" = {};
+      };
+    };
+  };
+
+  systemd.services.btrbk-btrbk = {
+    preStart = ''
+      sudo mount /dev/lvm/root /mnt
+    '';
+    postStop = ''
+      sudo umount /mnt
+    '';
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = ["btrbk"];
+      commands = [
+        {
+          command = "/run/wrappers/bin/mount";
+          options = ["NOPASSWD"];
+        }
+        {
+          command = "/run/wrappers/bin/umount";
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+}
