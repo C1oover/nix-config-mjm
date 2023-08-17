39165a640574ddf64dad2422da7f372b886e516c
Matt Moriarity <matt@mattmoriarity.com>
2023-08-17 10:00:09 -0600

    xdg: set directory names and create automatically

--- home/matt/features/xdg/default.nix
+++ home/matt/features/xdg/default.nix
@@ -1,12 +1,23 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
   xdg = {
     enable = true;
     userDirs = lib.mkIf pkgs.stdenv.isLinux {
       enable = true;
+      createDirectories = true;
+
+      desktop = "${config.home.homeDirectory}/desktop";
+      documents = "${config.home.homeDirectory}/documents";
+      download = "${config.home.homeDirectory}/downloads";
+      music = null;
+      pictures = "${config.home.homeDirectory}/pictures";
+      publicShare = null;
+      templates = null;
+      videos = null;
     };
   };
 }
