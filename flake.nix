8e170cf65f84eb80cff63c86bc5e57250236c964
Matt Moriarity <matt@mattmoriarity.com>
2023-09-09 22:17:12 -0600

    pull ci-flake-update app into its own file

--- flake.nix
+++ flake.nix
@@ -107,37 +107,7 @@
         apps.ci-flake-update.program = lib.getExe (pkgs.writeShellApplication {
           name = "ci-flake-update";
           runtimeInputs = with pkgs; [curl jq nix git];
-          text = ''
-            function latest_nixpkgs() {
-              curl -s 'https://monitoring.nixos.org/prometheus/api/v1/query?query=channel_revision%7Bchannel%3D%22nixpkgs-unstable%22%7D' \
-                | jq -r '.data.result[0].metric.revision'
-            }
-
-            function my_nixpkgs() {
-              nixpkgs_name="$(nix flake metadata . --json | jq -r '.locks.nodes.root.inputs.nixpkgs')"
-              nix flake metadata . --json | jq -r ".locks.nodes.$nixpkgs_name.locked.rev"
-            }
-
-            latest="$(latest_nixpkgs)"
-            mine="$(my_nixpkgs)"
-
-            echo "latest: $latest"
-            echo "mine: $mine"
-
-            if [ "$latest" = "$mine" ]; then
-              echo "no updates: all done"
-              exit 0
-            fi
-
-            echo "latest nixpkgs doesn't match my version. updating..."
-            nix flake update
-            git config user.email "gitlab@matt.mattmoriarity.com"
-            git config user.name "GitLab Automation"
-            git add flake.lock
-            git commit -m "nix flake update"
-            git remote add gitlab "https://ci:$FLAKE_UPDATE_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git"
-            git push gitlab HEAD:main
-          '';
+          text = builtins.readFile ./flake-update.sh;
         });
       };
     };
