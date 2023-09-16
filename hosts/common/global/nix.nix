71183400d74814a5945acc5de5f7e03b0b006d1c
Matt Moriarity <matt@mattmoriarity.com>
2023-09-16 02:09:10 +0000

    nix: add helix cachix

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -1,5 +1,13 @@
 {inputs, ...}: {
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://helix.cachix.org"
+    ];
+    trusted-public-keys = [
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+    ];
+  };
 
   nix.registry = {
     nixpkgs.flake = inputs.nixpkgs;
