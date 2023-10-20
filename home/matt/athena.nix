c38d2bf6257bc5c2db44a9f95cdf6dd99198f7c9
Matt Moriarity <matt@slab.com>
2023-10-20 14:11:12 -0600

    athena: bump slab elixir-ls to 1.17.3

--- home/matt/athena.nix
+++ home/matt/athena.nix
@@ -178,9 +178,10 @@ in {
       # use an official elixir-ls release so that it just runs with whatever elixir version
       # is in the environment. since we use asdf for the version, we can't ensure the elixir
       # version in nixpkgs matches.
+      version = "0.17.3";
       elixir-ls = pkgs.fetchzip {
-        url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v0.17.1/elixir-ls-v0.17.1.zip";
-        hash = "sha256-KdQDDZfj6Je78dPlw2YZgcSL5pdehq+CV/L5lVrRthM=";
+        url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v${version}/elixir-ls-v${version}.zip";
+        hash = "sha256-X5PABhG+tIgBN6cCb3D/0T+qgycuhV+tdAq19VLZJFk=";
         stripRoot = false;
       };
     in "${elixir-ls}/language_server.sh";
