22059505b4f82568808ea8c78251b89895abce88
Matt Moriarity <matt@mattmoriarity.com>
2023-10-20 23:29:24 -0600

    remove beeper package

--- packages/default.nix
+++ packages/default.nix
@@ -4,15 +4,9 @@
       inherit system;
       config.allowUnfree = true;
     };
-    lib = pkgs.lib;
   in {
     packages = {
-      beeper = pkgs.callPackage ./beeper.nix {};
       pragmata-pro = pkgs.callPackage ./pragmata-pro.nix {};
     };
-
-    apps.update-beeper.program = toString (pkgs.writeShellScript "update-beeper" ''
-      ${lib.getExe pkgs.nix} build .#beeper.updateScript && ./result
-    '');
   };
 }
