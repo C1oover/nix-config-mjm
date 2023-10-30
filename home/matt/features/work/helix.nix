b88904fcdadc32133f7e6129f3a351e5d53ac1c7
Matt Moriarity <matt@slab.com>
2023-10-30 09:52:59 -0600

    athena: update elixir-ls to 0.17.5

--- home/matt/features/work/helix.nix
+++ home/matt/features/work/helix.nix
@@ -15,10 +15,10 @@ in {
       # use an official elixir-ls release so that it just runs with whatever elixir version
       # is in the environment. since we use asdf for the version, we can't ensure the elixir
       # version in nixpkgs matches.
-      version = "0.17.3";
+      version = "0.17.5";
       elixir-ls = pkgs.fetchzip {
         url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v${version}/elixir-ls-v${version}.zip";
-        hash = "sha256-X5PABhG+tIgBN6cCb3D/0T+qgycuhV+tdAq19VLZJFk=";
+        hash = "sha256-hhPUCGxYIp2IQu47xhAoTLjYHAytj4PW38OkQJt2xy4=";
         stripRoot = false;
       };
     in "${elixir-ls}/language_server.sh";
