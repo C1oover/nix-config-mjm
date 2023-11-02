9c89945ea53fbc7969341b7de157ad9dce980153
Matt Moriarity <matt@mattmoriarity.com>
2023-11-01 19:16:58 -0600

    persephone: remove non-systemd initrd impermanence script

--- hosts/persephone/impermanence.nix
+++ hosts/persephone/impermanence.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{
   age.identityPaths = ["/persist/etc/ssh/ssh_host_ed25519_key"];
 
   environment.etc = {
@@ -22,7 +22,6 @@
     wantedBy = ["initrd.target"];
     after = ["dev-lvm-root.device"];
     requires = ["dev-lvm-root.device"];
-    # after = ["systemd-cryptsetup@cryptroot.service"];
     before = ["sysroot.mount"];
     unitConfig.DefaultDependencies = "no";
     serviceConfig.Type = "oneshot";
@@ -45,23 +44,4 @@
       umount /mnt
     '';
   };
-
-  boot.initrd.postDeviceCommands = lib.mkBefore ''
-    mkdir -p /mnt
-    mount -o subvol=/ /dev/lvm/root /mnt
-
-    btrfs subvolume list -o /mnt/root |
-    cut -f9 -d' ' |
-    while read subvolume; do
-      echo "deleting /$subvolume subvolume..."
-      btrfs subvolume delete "/mnt/$subvolume"
-    done &&
-    echo "deleting /root subvolume..." &&
-    btrfs subvolume delete /mnt/root
-
-    echo "restoring blank /root subvolume..."
-    btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-    umount /mnt
-  '';
 }
