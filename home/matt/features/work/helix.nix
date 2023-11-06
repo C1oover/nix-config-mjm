ace8b5b6fd4cb8716f46959cc16b056c0cd15d95
Matt Moriarity <matt@slab.com>
2023-11-06 08:26:35 -0700

    work: set up efm-langserver for credo

--- home/matt/features/work/helix.nix
+++ home/matt/features/work/helix.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: let
+  yamlFormat = pkgs.formats.yaml {};
   tomlFormat = pkgs.formats.toml {};
 in {
   imports = [
@@ -8,6 +9,35 @@ in {
   # cmake is needed to build some elixir deps
   # if it's not in the path, elixir-ls might just not work
   home.packages = with pkgs; [cmake];
+
+  programs.helix = let
+    efmConfig = yamlFormat.generate "efm-config.yml" {
+      version = 2;
+      root-markers = [".git/"];
+      languages.elixir = [
+        {
+          lint-command = "mix credo suggest --format=flycheck --read-from-stdin";
+          lint-stdin = true;
+          lint-formats = [
+            "%f:%l:%c: %t: %m"
+            "%f:%l: %t: %m"
+          ];
+          root-markers = [
+            "mix.lock"
+            "mix.exs"
+          ];
+        }
+      ];
+    };
+  in {
+    extraPackages = with pkgs; [efm-langserver];
+    languages = {
+      language-server.efm = {
+        command = "efm-langserver";
+        args = ["-c" "${efmConfig}"];
+      };
+    };
+  };
 
   # manually link this into ~/Projects/slab/.helix/languages.toml
   xdg.configFile."helix/slab/languages.toml".source = tomlFormat.generate "slab-languages.toml" {
@@ -22,5 +52,18 @@ in {
         stripRoot = false;
       };
     in "${elixir-ls}/language_server.sh";
+
+    language = [
+      {
+        name = "elixir";
+        language-servers = [
+          {
+            name = "efm";
+            only-features = ["diagnostics"];
+          }
+          {name = "elixir-ls";}
+        ];
+      }
+    ];
   };
 }
