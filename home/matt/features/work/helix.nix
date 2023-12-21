63e023bd8b30e7e87a0a4773c2bf7f5a78bd0a5e
Matt Moriarity <matt@slab.com>
2023-12-21 13:29:55 -0700

    work: update elixir-ls to 1.18.1

--- home/matt/features/work/helix.nix
+++ home/matt/features/work/helix.nix
@@ -45,10 +45,10 @@ in {
       # use an official elixir-ls release so that it just runs with whatever elixir version
       # is in the environment. since we use asdf for the version, we can't ensure the elixir
       # version in nixpkgs matches.
-      version = "0.17.9";
+      version = "0.18.1";
       elixir-ls = pkgs.fetchzip {
         url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v${version}/elixir-ls-v${version}.zip";
-        hash = "sha256-q0H4VlV8xjMMZf82VHKB+v7bxEVZUDpBb2up3AfYPqc=";
+        hash = "sha256-i4oBD2/ePBjZ3NfrFnVC19ztWquQiLKeMqqMeenops4=";
         stripRoot = false;
       };
     in "${elixir-ls}/language_server.sh";
