2c361c2cffbef9fd97268a33529e4ca51fc43cb0
Matt Moriarity <matt@mattmoriarity.com>
2023-12-20 20:42:52 -0700

    arges: add impermanence

--- hosts/arges/hardware-configuration.nix
+++ hosts/arges/hardware-configuration.nix
@@ -8,12 +8,26 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+    options = ["defaults" "mode=755"];
+  };
+
+  fileSystems."/nix" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
     options = ["noatime"];
+    neededForBoot = true;
   };
 
-  swapDevices = [];
+  fileSystems."/boot" = {
+    device = "/nix/boot";
+    options = ["bind" "X-fstrim.notrim"];
+  };
+
+  swapDevices = [
+    {device = "/nix/swap";}
+  ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
---
+++ hosts/arges/impermanence.nix
@@ -0,0 +1,28 @@
+{inputs, ...}: {
+  imports = [
+    inputs.impermanence.nixosModules.impermanence
+  ];
+
+  # TODO remove later
+  boot.initrd.preFailCommands = "allowShell=1";
+
+  age.identityPaths = ["/nix/persist/etc/ssh/ssh_host_ed25519_key"];
+
+  environment.persistence."/nix/persist" = {
+    directories = [
+      # TODO remove this later by setting up nix config to join tailscale automatically
+      "/var/lib/tailscale"
+    ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
+  };
+
+  security.sudo.extraConfig = ''
+    Defaults lecture = never
+  '';
+}
