048e7bdc3476a66d598177618112861b35582f83
Matt Moriarity <matt@mattmoriarity.com>
2023-12-24 08:54:59 -0700

    postgresql: remove old consul auth entry

--- hosts/themis/services/postgresql.nix
+++ hosts/themis/services/postgresql.nix
@@ -15,8 +15,6 @@
       # IPv6 local connections:
       host    all             all             ::1/128                 trust
 
-      host	all		consul		10.0.2.10/32		trust
-
       # allow connections from the local network
       host	all		all		10.0.0.0/8		scram-sha-256
       host  all   all   2601:282:167f:3eec::/64  scram-sha-256
