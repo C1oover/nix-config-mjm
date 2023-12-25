d3da248417f7cb9f5dcb81af13c3d0cb9c5e6d16
Matt Moriarity <matt@mattmoriarity.com>
2023-12-24 18:27:34 -0700

    arges: put the filesystems back

--- hosts/arges/hardware-configuration.nix
+++ hosts/arges/hardware-configuration.nix
@@ -8,26 +8,34 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = ["defaults" "mode=755"];
-  };
-
-  fileSystems."/nix" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
     options = ["noatime"];
-    neededForBoot = true;
   };
 
-  fileSystems."/boot" = {
-    device = "/nix/boot";
-    options = ["bind" "X-fstrim.notrim"];
-  };
+  swapDevices = [];
 
-  swapDevices = [
-    {device = "/nix/swap";}
-  ];
+  # fileSystems."/" = {
+  #   device = "none";
+  #   fsType = "tmpfs";
+  #   options = ["defaults" "mode=755"];
+  # };
+
+  # fileSystems."/nix" = {
+  #   device = "/dev/disk/by-label/NIXOS_SD";
+  #   fsType = "ext4";
+  #   options = ["noatime"];
+  #   neededForBoot = true;
+  # };
+
+  # fileSystems."/boot" = {
+  #   device = "/nix/boot";
+  #   options = ["bind" "X-fstrim.notrim"];
+  # };
+
+  # swapDevices = [
+  #   {device = "/nix/swap";}
+  # ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
