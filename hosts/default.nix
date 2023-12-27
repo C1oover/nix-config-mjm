8cfb284f88e091d38d6be0d7803374732dffbbd1
Matt Moriarity <matt@mattmoriarity.com>
2023-12-27 16:57:44 -0700

    chaos: deploy automatically

--- hosts/default.nix
+++ hosts/default.nix
@@ -203,7 +203,7 @@ in {
         trap finish EXIT
 
         if [ "$ARCH" = "x86_64" ]; then
-          targets=(.#aion .#alecto .#cronus .#gaia .#helios .#megaera .#nemesis .#orion .#phoebe .#rhea .#thanatos .#themis .#tisiphone .#hypnos)
+          targets=(.#aion .#alecto .#cronus .#chaos .#gaia .#helios .#megaera .#nemesis .#orion .#phoebe .#rhea .#thanatos .#themis .#tisiphone .#hypnos)
         elif [ "$ARCH" = "arm64" ]; then
           # deploy to arges first, because it may need to reload the gitlab-runner, which fails if
           # the ingress is unavailable, which might temporarily happen when deploying to the other
