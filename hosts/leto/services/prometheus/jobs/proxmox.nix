96519e2bc7b43290fb97c82b24031c06b413c7d6
Matt Moriarity <matt@mattmoriarity.com>
2024-01-06 23:39:21 -0700

    prometheus: only scrape a single proxmox instance

--- hosts/leto/services/prometheus/jobs/proxmox.nix
+++ hosts/leto/services/prometheus/jobs/proxmox.nix
@@ -3,25 +3,13 @@
     {
       job_name = "proxmox";
       metrics_path = "/pve";
+      params = {
+        target = ["proxmox.service.consul"];
+      };
       consul_sd_configs = [
         {
           server = "127.0.0.1:8500";
-          services = ["proxmox"];
-        }
-      ];
-      relabel_configs = [
-        {
-          source_labels = ["__address__"];
-          regex = "(.+):\\d+";
-          target_label = "__param_target";
-        }
-        {
-          source_labels = ["__param_target"];
-          target_label = "instance";
-        }
-        {
-          target_label = "__address__";
-          replacement = "pve-exporter.service.consul:9221";
+          services = ["pve-exporter"];
         }
       ];
     }
