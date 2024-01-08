555839bb58f7f5254b40cdbb7db83ee1212e2c73
Matt Moriarity <matt@mattmoriarity.com>
2024-01-07 17:04:01 -0700

    prometheus: remove approle and vault policies

--- apps/prometheus/default.nix
+++ apps/prometheus/default.nix
@@ -1,31 +1,4 @@
 {
-  vault.approles.roles.prometheus = {
-    tokenPolicies = ["prometheus" "alertmanager"];
-  };
-
-  vault.policies.prometheus.text = ''
-    # Allow Prometheus to scrape Vault's metrics endpoint
-    path "sys/metrics" {
-      capabilities = ["read", "list"]
-    }
-
-    # Allow Prometheus to issue itself client certificates for accessing Nomad's metrics
-    path "pki-int/issue/nomad-cluster" {
-      capabilities = ["update"]
-    }
-  '';
-
-  vault.policies.alertmanager.text = ''
-    # Allow alertmanager to read Pushover secrets for sending notifications
-    path "kv/data/pushover" {
-      capabilities = ["read"]
-    }
-
-    path "kv/data/pagerduty" {
-      capabilities = ["read"]
-    }
-  '';
-
   ingress.virtualHosts = {
     alertmanager = {
       upstream.service.name = "alertmanager";
