d7168666cb1b828ddec7e951ffa86d3a27ef7467
Matt Moriarity <matt@mattmoriarity.com>
2024-01-07 17:31:34 -0700

    netbox: allow netbox.midna.dev

--- hosts/leto/services/netbox.nix
+++ hosts/leto/services/netbox.nix
@@ -14,6 +14,7 @@
     listenAddress = "[::]";
     settings = {
       ALLOWED_HOSTS = [
+        "netbox.midna.dev"
         "netbox.home.mattmoriarity.com"
         "netbox.service.consul"
         "10.0.2.41"
@@ -33,9 +34,11 @@
       };
       CORS_ORIGIN_ALLOW_ALL = false;
       CORS_ORIGIN_WHITELIST = [
+        "https://netbox.midna.dev"
         "https://netbox.home.mattmoriarity.com"
       ];
       CSRF_TRUSTED_ORIGINS = [
+        "https://netbox.midna.dev"
         "https://netbox.home.mattmoriarity.com"
         "http://netbox.service.consul:8000"
       ];
