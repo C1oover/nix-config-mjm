cffd489c42c67a9aac31050d416851ac27b5b419
Matt Moriarity <matt@mattmoriarity.com>
2024-01-08 20:51:56 -0700

    authelia: remove home.mattmoriarity.com instance

--- hosts/leto/services/authelia.nix
+++ hosts/leto/services/authelia.nix
@@ -19,8 +19,6 @@
       public = false;
       authorization_policy = "two_factor";
       redirect_uris = [
-        "https://vault.home.mattmoriarity.com/oidc/callback"
-        "https://vault.home.mattmoriarity.com/ui/vault/auth/oidc/oidc/callback"
         "https://vault.midna.dev/oidc/callback"
         "https://vault.midna.dev/ui/vault/auth/oidc/oidc/callback"
         "http://localhost:8250/oidc/callback"
@@ -39,7 +37,6 @@
         "https://10.0.2.11:8006"
         "https://artemis.home.mattmoriarity.com:8006"
         "https://apollo.home.mattmoriarity.com:8006"
-        "https://proxmox.home.mattmoriarity.com"
         "https://proxmox.midna.dev"
       ];
       scopes = ["openid" "profile" "email"];
@@ -52,7 +49,6 @@
       public = false;
       authorization_policy = "two_factor";
       redirect_uris = [
-        "https://minio-console.home.mattmoriarity.com/oauth_callback"
         "https://minio-console.midna.dev/oauth_callback"
       ];
       scopes = ["openid" "profile" "groups" "email"];
@@ -65,7 +61,7 @@ in {
   services.authelia.instances.main = {
     enable = true;
     settings = {
-      default_redirection_url = "https://homelab.home.mattmoriarity.com/";
+      default_redirection_url = "https://home.midna.dev/";
       default_2fa_method = "webauthn";
       server.host = "::";
       telemetry.metrics = {
@@ -73,70 +69,6 @@ in {
         address = "tcp://0.0.0.0:9959";
       };
       webauthn.display_name = "Homelab";
-      authentication_backend.ldap = {
-        implementation = "custom";
-        url = "ldap://localhost:3890";
-        timeout = "5s";
-        start_tls = false;
-        base_dn = "dc=home,dc=mattmoriarity,dc=com";
-        username_attribute = "uid";
-        additional_users_dn = "ou=people";
-        users_filter = "(&({username_attribute}={input})(objectClass=person))";
-        additional_groups_dn = "ou=groups";
-        groups_filter = "(member={dn})";
-        group_name_attribute = "cn";
-        mail_attribute = "mail";
-        display_name_attribute = "displayName";
-        user = "uid=service,ou=people,dc=home,dc=mattmoriarity,dc=com";
-      };
-      access_control.default_policy = "two_factor";
-      session.domain = "home.mattmoriarity.com";
-      session.redis = {
-        host = "redis.service.consul";
-        port = 6379;
-      };
-      storage.postgres = {
-        host = "postgresql.service.consul";
-        port = 5432;
-        database = "authelia";
-      };
-      notifier.smtp = {
-        host = "smtp.fastmail.com";
-        port = 587;
-        username = "matt@mattmoriarity.com";
-        sender = "Authelia <admin@mattmoriarity.com>";
-      };
-      identity_providers.oidc.clients = oidcClients;
-    };
-    secrets = {
-      jwtSecretFile = config.age.secrets."authelia-jwt-secret".path;
-      oidcHmacSecretFile = config.age.secrets."authelia-hmac-secret".path;
-      oidcIssuerPrivateKeyFile = config.age.secrets."authelia-jwt-private-key".path;
-      sessionSecretFile = config.age.secrets."authelia-session-secret".path;
-      storageEncryptionKeyFile = config.age.secrets."authelia-storage-encryption-key".path;
-    };
-    environmentVariables = {
-      AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE = config.age.secrets."authelia-ldap-password".path;
-      AUTHELIA_NOTIFIER_SMTP_PASSWORD_FILE = config.age.secrets."authelia-smtp-password".path;
-    };
-    settingsFiles = [
-      "/run/secrets/authelia/db-config.yml"
-    ];
-  };
-
-  services.authelia.instances.external = {
-    inherit user group;
-    enable = true;
-    settings = {
-      default_redirection_url = "https://git.midna.dev/";
-      default_2fa_method = "webauthn";
-      server.host = "::";
-      server.port = 9092;
-      telemetry.metrics = {
-        enabled = true;
-        address = "tcp://0.0.0.0:9960";
-      };
-      webauthn.display_name = "Homelab External";
       authentication_backend.ldap = {
         implementation = "custom";
         url = "ldap://localhost:3890";
@@ -197,12 +129,11 @@ in {
       session.redis = {
         host = "redis.service.consul";
         port = 6379;
-        database_index = 3;
       };
       storage.postgres = {
         host = "postgresql.service.consul";
         port = 5432;
-        database = "authelia_external";
+        database = "authelia";
       };
       notifier.smtp = {
         host = "smtp.fastmail.com";
@@ -229,7 +160,6 @@ in {
   };
 
   systemd.services.authelia-main.after = ["lldap.service"];
-  systemd.services.authelia-external.after = ["lldap.service"];
 
   networking.firewall.allowedTCPPorts = [
     9091
@@ -256,24 +186,6 @@ in {
     ];
   };
 
-  services.consul.services.authelia-external = {
-    port = 9092;
-
-    meta = {
-      metrics_path = "/metrics";
-      metrics_port = "9960";
-    };
-
-    checks = [
-      {
-        name = "authelia is ready";
-        http = "http://localhost:9092/api/health";
-        interval = "30s";
-        timeout = "5s";
-      }
-    ];
-  };
-
   systemd.tmpfiles.rules = ["d /run/secrets/authelia 0700 ${user} ${group} - -"];
 
   services.vault-agent.instances.main.templates = [
@@ -287,7 +199,7 @@ in {
         {{ end }}
       '';
       destination = "/run/secrets/authelia/db-config.yml";
-      command = "systemctl restart authelia-main.service authelia-external.service";
+      command = "systemctl restart authelia-main.service";
     }
   ];
 
