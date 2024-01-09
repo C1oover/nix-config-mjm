33f1213c94f17aecc165ef3f79fb79f9d08682e9
Matt Moriarity <matt@mattmoriarity.com>
2024-01-08 19:42:14 -0700

    prometheus: remove dns private cname job

--- hosts/leto/services/prometheus/jobs/default.nix
+++ hosts/leto/services/prometheus/jobs/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./blackbox-dns-ad-blocking.nix
     ./blackbox-dns-private.nix
-    ./blackbox-dns-private-cname.nix
     ./blackbox-dns-public.nix
     ./consul-agent.nix
     ./consul-connect-envoy.nix
