e637db736c56d730ccdaf21d7e461741b42b79e4
Matt Moriarity <matt@mattmoriarity.com>
2024-01-13 11:15:14 -0700

    exportarr-sonarr: init

--- hosts/chaos/services/arr.nix
+++ hosts/chaos/services/arr.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   services.sonarr = {
     enable = true;
     openFirewall = true;
@@ -11,9 +15,23 @@
   };
   users.users.radarr.extraGroups = ["media"];
 
+  services.prometheus.exporters = {
+    exportarr-sonarr = {
+      enable = true;
+      openFirewall = true;
+      apiKeyFile = config.age.secrets."sonarr-apikey".path;
+      url = "http://127.0.0.1:8989";
+    };
+  };
+
   services.consul.services = {
     sonarr = {
       port = 8989;
+
+      meta = {
+        metrics_path = "/metrics";
+        metrics_port = toString config.services.prometheus.exporters.exportarr-sonarr.port;
+      };
 
       checks = [
         {
@@ -45,4 +63,8 @@
 
   # ffprobe
   systemd.services.radarr.path = [pkgs.ffmpeg];
+
+  age.secrets = {
+    "sonarr-apikey".file = ../../../secrets/sonarr-apikey.age;
+  };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -33,6 +33,7 @@ in
     "smb-creds.age".publicKeys = personalKeys ++ [persephone];
     "smb-creds-server.age".publicKeys = personalKeys ++ [chaos];
     "sabnzbd-apikey.age".publicKeys = personalKeys ++ [chaos];
+    "sonarr-apikey.age".publicKeys = personalKeys ++ [chaos];
 
     "nut-primary-password.age".publicKeys = personalKeys ++ [arges];
     "nut-secondary-password.age".publicKeys = personalKeys ++ allNixOS;
