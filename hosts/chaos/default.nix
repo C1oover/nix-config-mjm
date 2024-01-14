04b73c417a69a96aaca9d314e709e1480f153806
Matt Moriarity <matt@mattmoriarity.com>
2024-01-14 11:16:31 -0700

    jellyfin: fix ffmpeg build

--- hosts/chaos/default.nix
+++ hosts/chaos/default.nix
@@ -15,6 +15,14 @@
     ./services/arr.nix
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      jellyfin-ffmpeg = prev.jellyfin-ffmpeg.overrideAttrs (old: {
+        configureFlags = builtins.filter (f: f != "--enable-libaribcaption") old.configureFlags;
+      });
+    })
+  ];
+
   networking.hostName = "chaos";
 
   boot.supportedFilesystems = ["xfs"];
