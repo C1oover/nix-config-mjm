10b1ead951e1b4f36cd00762990dd5678645c7eb
Matt Moriarity <matt@mattmoriarity.com>
2024-01-21 00:10:03 -0700

    taskwarrior: remove add laundry command

--- home/matt/features/taskwarrior/default.nix
+++ home/matt/features/taskwarrior/default.nix
@@ -3,17 +3,10 @@
   lib,
   config,
   ...
-}: let
-  task-add-laundry = pkgs.writeShellApplication {
-    name = "task-add-laundry";
-    runtimeInputs = with pkgs; [taskwarrior coreutils];
-    text = builtins.readFile ./task-add-laundry.sh;
-  };
-in {
+}: {
   home.packages = with pkgs; [
     vit
     taskwarrior-tui
-    task-add-laundry
   ];
 
   programs.taskwarrior = {
