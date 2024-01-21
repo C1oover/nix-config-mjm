180f20c8e152c03572df4c47ec7db08a59d3c729
Matt Moriarity <matt@mattmoriarity.com>
2024-01-20 19:23:32 -0700

    desktop: use resholve

--- home/matt/features/desktop/default.nix
+++ home/matt/features/desktop/default.nix
@@ -2,24 +2,14 @@
   pkgs,
   inputs,
   ...
-}: let
-  # set brightness and volume for night
-  nightMode = pkgs.writeShellApplication {
-    name = "night-mode";
-    runtimeInputs = with pkgs; [light pulseaudio];
-    text = ''
-      pactl set-sink-volume @DEFAULT_SINK@ 30%
-      light -S 1
-    '';
-  };
-in {
+}: {
   imports = [
     ./rc.nix
   ];
 
   home.packages = builtins.attrValues {
-    inherit nightMode;
     inherit (pkgs) imv wl-clipboard xdg-utils zeal;
+    inherit (pkgs.callPackages ./scripts.nix {}) night-mode;
     inherit (inputs.kde2nix.packages.${pkgs.system}) kbreakout kmahjongg kmines kpat palapeli;
     inherit (inputs.plasma-manager.packages.${pkgs.system}) rc2nix;
   };
---
+++ home/matt/features/desktop/scripts.nix
@@ -0,0 +1,17 @@
+{
+  bash,
+  resholve,
+  light,
+  pulseaudio,
+}: let
+  interpreter = "${bash}/bin/bash";
+in {
+  night-mode =
+    resholve.writeScriptBin "night-mode" {
+      inherit interpreter;
+      inputs = [light pulseaudio];
+    } ''
+      pactl set-sink-volume @DEFAULT_SINK@ 30%
+      light -S 1
+    '';
+}
