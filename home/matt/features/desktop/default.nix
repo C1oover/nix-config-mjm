0369962e7d0f2620b5b1ba1a3e7e9bba9dbe340b
Matt Moriarity <matt@mattmoriarity.com>
2024-01-23 00:08:57 -0700

    desktop: add bitwarden

--- home/matt/features/desktop/default.nix
+++ home/matt/features/desktop/default.nix
@@ -8,7 +8,7 @@
   ];
 
   home.packages = builtins.attrValues {
-    inherit (pkgs) imv wl-clipboard xdg-utils zeal;
+    inherit (pkgs) bitwarden imv wl-clipboard xdg-utils zeal;
     inherit (pkgs.callPackages ./scripts.nix {}) night-mode;
     inherit (inputs.kde2nix.packages.${pkgs.system}) kbreakout kmahjongg kmines kpat palapeli;
     inherit (inputs.plasma-manager.packages.${pkgs.system}) rc2nix;
