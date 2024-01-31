530059acaa6fbd2df581952341af2b07ff7d0b8d
Matt Moriarity <matt@mattmoriarity.com>
2024-01-31 14:34:55 -0700

    nixos: set user full name

--- hosts/common/global/nixos/default.nix
+++ hosts/common/global/nixos/default.nix
@@ -31,6 +31,7 @@
   users.mutableUsers = false;
   users.users.matt = {
     isNormalUser = true;
+    description = "MJ";
     extraGroups = ["wheel"];
     shell = pkgs.zsh;
     hashedPassword = "$6$JhSUuIask83mtadB$iV5I3SmQE13rVV08RpCN4Ho09VvpmCm6xouZ2o7/1rhR63YFh/WtLAdM1f2P4hgJrxi.ss2zh53xpbSqx/zy9/";
