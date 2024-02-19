91992cad5ea8c37fa04ca049dbcc8347c893284c
Matt Moriarity <matt@mattmoriarity.com>
2024-02-19 14:33:58 -0700

    remove fprint pam reordering

--- hosts/persephone/default.nix
+++ hosts/persephone/default.nix
@@ -80,15 +80,6 @@
 
   virtualisation.podman.enable = true;
 
-  # move fprintd after unix auth so that it's possible to unlock
-  # by either entering a password or with fingerprint
-  security.pam.services.polkit-1.rules.auth.fprintd.order =
-    config.security.pam.services.polkit-1.rules.auth.unix.order + 5;
-  security.pam.services.login.rules.auth.fprintd.order =
-    config.security.pam.services.login.rules.auth.unix.order + 5;
-  security.pam.services.kde.rules.auth.fprintd.order =
-    config.security.pam.services.kde.rules.auth.unix.order + 5;
-
   console = {
     font = "${pkgs.terminus_font}/share/consolefonts/ter-u32n.psf.gz";
     keyMap = "us";
