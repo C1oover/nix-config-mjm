95ff75ecd077fa58c3b865cc2a27dde7db640547
Matt Moriarity <matt@mattmoriarity.com>
2024-02-22 20:46:44 -0700

    tempo: add health check

--- hosts/leto/services/tempo.nix
+++ hosts/leto/services/tempo.nix
@@ -45,6 +45,15 @@
 
   services.consul.services.tempo = {
     port = 3200;
+
+    checks = [
+      {
+        name = "tempo is ready";
+        http = "http://localhost:3200/ready";
+        interval = "15s";
+        timeout = "10s";
+      }
+    ];
   };
 
   networking.firewall.allowedTCPPorts = [
