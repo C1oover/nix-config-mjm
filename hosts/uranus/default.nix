f24b2ca1dac31c240797bcf68f7f7c6c11d2e865
Matt Moriarity <matt@mattmoriarity.com>
2024-02-25 17:52:26 -0700

    uranus: fix nouveau specialisation

--- hosts/uranus/default.nix
+++ hosts/uranus/default.nix
@@ -28,19 +28,6 @@
     };
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # needed for wayland to work at all
-  hardware.nvidia.modesetting.enable = true;
-
-  hardware.opengl.extraPackages = [
-    pkgs.libvdpau-va-gl
-    pkgs.nvidia-vaapi-driver
-  ];
-
-  # without this, discord won't run
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
   # Allow desktop mouse and keyboard to wake the system
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c24a", ATTR{power/wakeup}="enabled"
