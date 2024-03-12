30a75e1995a1d1eec3bd48c743b6a60c10375285
Matt Moriarity <matt@mattmoriarity.com>
2024-03-11 22:49:11 -0600

    desktop: install plasmatube

--- home/matt/features/desktop/default.nix
+++ home/matt/features/desktop/default.nix
@@ -37,7 +37,7 @@ in
         zeal
         ;
       inherit (pkgs.callPackages ./scripts.nix { }) night-mode;
-      inherit (pkgs.kdePackages) kcalc;
+      inherit (pkgs.kdePackages) kcalc plasmatube;
       inherit (inputs.plasma-manager.packages.${pkgs.system}) rc2nix;
     };
 
