911c8548f85da6471fd88cf2b0628b2f37d5f37a
Matt Moriarity <matt@mattmoriarity.com>
2024-03-19 18:00:04 -0600

    desktop: set sddm wallpaper

--- hosts/common/optional/desktop/default.nix
+++ hosts/common/optional/desktop/default.nix
@@ -27,6 +27,10 @@ in
     environment.systemPackages = [
       pkgs.kdePackages.kpmcore
       pkgs.kdePackages.partitionmanager
+      (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+        [General]
+        background=${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/MilkyWay/contents/images/5120x2880.png
+      '')
     ];
 
     fonts = {
