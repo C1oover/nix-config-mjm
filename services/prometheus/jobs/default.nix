cc38152c82afda915eaecb3e3423f32d13fb3d64
Matt Moriarity <matt@mattmoriarity.com>
2024-03-20 22:21:38 -0600

    prometheus: remove defunct jobs

--- services/prometheus/jobs/default.nix
+++ services/prometheus/jobs/default.nix
@@ -4,12 +4,10 @@
     ./blackbox-dns-private.nix
     ./blackbox-dns-public.nix
     ./consul-agent.nix
-    ./consul-connect-envoy.nix
     ./consul-exporter.nix
     ./consul-services.nix
     ./homelab-https.nix
     ./proxmox.nix
-    ./pushgateway.nix
     ./vault.nix
   ];
 }
