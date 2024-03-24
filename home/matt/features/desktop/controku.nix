1f830947ea3b91d9c2909e8e606ada5cd2458450
Matt Moriarity <matt@mattmoriarity.com>
2024-03-23 19:17:19 -0600

    desktop(controku): fix ip

--- home/matt/features/desktop/controku.nix
+++ home/matt/features/desktop/controku.nix
@@ -21,7 +21,7 @@ in
     home.file."${config.xdg.cacheHome}/controku/devices.json".text = builtins.toJSON [
       {
         name = ''55" TCL Roku TV'';
-        ip = "10.1.0.111";
+        ip = "10.0.1.111";
       }
     ];
   };
