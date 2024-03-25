96046445297a37e80077c3955c13755f2a16f52b
Matt Moriarity <matt@mattmoriarity.com>
2024-03-24 18:08:32 -0600

    leto: use inline encrypted secret id

--- hosts/leto/default.nix
+++ hosts/leto/default.nix
@@ -65,6 +65,16 @@
   };
 
   vault-secrets.roleId = "29829ea8-3eb2-b3d6-8aab-d150dbb48e3d";
+  vault-secrets.encryptedSecretId = ''
+    DHzAexF2RZGcSwvqCLwg/iAAAAABAAAADAAAABAAAABSZJNHWPDHfdUl5GEAAAAAgAAAAAAAAAALACM
+    A8AAAACAAAAAAngAgcg69FO6u/yrDbYmK7AuWSZoNAvTWwXcUFFwhzqxDpNcAEFQxPzzc1Lrm+Q2P66
+    +DWzuRCdV/ecJDIjkmRI6oWxnx8nPqwB5ruWCO3ttkUyE4ipvWb+oOv8lCbpjobGt460hv6Mzc31E0n
+    +bo8u2E9Zc91fJCOi8Uw6Ib1/+OvuSFsnMlG1nA20/gVp/37xrPQWHQSrBzdCv3hfKQAE4ACAALAAAA
+    EgAg/928wzzwZljax6QULbex0pLe130yDZJRVcq/rrtdgRsAEAAgFo7mj/RX22cqv8C30ynLv/RmDhZ
+    NauqsclDrZCQWRV//3bzDPPBmWNrHpBQtt7HSkt7XfTINklFVyr+uu12BGwAAAAAaVaHXdHpVx90H6W
+    f2e4Z5JxE3E05AnRH8My5MbPwRe15IvtKP39fGKv6Q+QbD8WhIxALTqcjPDLUmqD5DJz2KD/C7y+Qft
+    uOEgE0Kq+B6xqdD/zrr
+  '';
 
   system.stateVersion = "24.05";
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -15,7 +15,6 @@ in
   "arges-approle-secret-id.age".publicKeys = personalKeys ++ [ arges ];
   "brontes-approle-secret-id.age".publicKeys = personalKeys ++ [ brontes ];
   "helios-approle-secret-id.age".publicKeys = personalKeys ++ [ helios ];
-  "leto-approle-secret-id.age".publicKeys = personalKeys ++ [ leto ];
   "steropes-approle-secret-id.age".publicKeys = personalKeys ++ [ steropes ];
 
   "ngrok.age".publicKeys = [
