01f9d3ee53e81d02469293e97434b6665f353009
Matt Moriarity <matt@mattmoriarity.com>
2024-04-03 16:34:49 -0600

    invidious: remove overlay as invidious is updated

--- services/media-server/invidious.nix
+++ services/media-server/invidious.nix
@@ -8,20 +8,6 @@ in
     mjm.postgresql.enable = true;
     mjm.state.directories = [ "/var/lib/private/invidious" ];
 
-    nixpkgs.overlays = [
-      (final: prev: {
-        invidious = prev.invidious.overrideAttrs (_oldAttrs: {
-          src = prev.fetchFromGitHub {
-            owner = "iv-org";
-            repo = "invidious";
-            fetchSubmodules = true;
-            rev = "08390acd0c17875fddb84cabba54197a5b5740e4";
-            sha256 = "sha256-75C/ImX/PYikVdSO4rZM/aYyEgx6pU90BHNeRFfcsDM=";
-          };
-        });
-      })
-    ];
-
     services.invidious = {
       enable = true;
       domain = "tube.midna.dev";
