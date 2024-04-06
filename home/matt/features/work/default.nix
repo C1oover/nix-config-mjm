0a842b35a145c8358b163791b9e8cb3de2725aba
Matt Moriarity <matt@mattmoriarity.com>
2024-04-06 12:55:14 -0600

    work: remove ngrok

--- home/matt/features/work/default.nix
+++ home/matt/features/work/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./git.nix
     ./helix.nix
-    ./ngrok.nix
   ];
 
   home.packages = builtins.attrValues {
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,5 @@
 with import ./keys.nix;
 {
-  "ngrok.age".publicKeys = [
-    matt-athena
-    athena
-  ];
-
   "newsboat-miniflux-token.age".publicKeys = personalKeys;
 
   "smb-creds.age".publicKeys = personalKeys ++ [ persephone ];
