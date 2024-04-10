5e4ac5fad91c7d95e690f2193a3dbdeae86d8fb2
Matt Moriarity <matt@mattmoriarity.com>
2024-04-09 22:21:30 -0600

    go back to using upstream bcachefs module
    fixes #25

--- modules/nixos/bcachefs.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, modulesPath, ... }:
-{
-  disabledModules = [ "${modulesPath}/tasks/filesystems/bcachefs.nix" ];
-
-  imports = [
-    "${inputs.nixos-bcachefs-clevis-systemd}/nixos/modules/tasks/filesystems/bcachefs.nix"
-  ];
-}
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,4 @@
 {
-  bcachefs = import ./bcachefs.nix;
   consul-services = import ./consul-services.nix;
   linkding = import ./linkding.nix;
   vault-agent = import ./vault-agent.nix;
