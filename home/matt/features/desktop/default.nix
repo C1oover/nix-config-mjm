4998aae6a04cdc51f9582a793c30e73cb33f5598
Matt Moriarity <matt@mattmoriarity.com>
2024-04-15 22:59:08 -0600

    desktop: remove neochat
    it's not currently building

--- home/matt/features/desktop/default.nix
+++ home/matt/features/desktop/default.nix
@@ -39,7 +39,11 @@ in
         zeal-qt6
         ;
       inherit (pkgs.callPackages ./scripts.nix { }) night-mode;
-      inherit (pkgs.kdePackages) kcalc neochat plasmatube;
+      inherit (pkgs.kdePackages)
+        kcalc
+        # neochat
+        plasmatube
+        ;
       inherit (inputs.plasma-manager.packages.${pkgs.system}) rc2nix;
     };
 
