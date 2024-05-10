33cfd6515e27d571c7533272ed78357f8be0d507
Matt Moriarity <matt@slab.com>
2024-05-10 13:03:18 -0600

    ci: create merge request when updating pins

--- scripts/ci-update-pins.sh
+++ scripts/ci-update-pins.sh
@@ -1,3 +1,10 @@
+existing_pr_count="$(curl "$CI_API_V4_URL/projects/$CI_PROJECT_ID/merge_requests?source_branch=npins-update&target_branch=main&state=opened" -H "Authorization: Bearer $PINS_UPDATE_TOKEN" | jq length)"
+
+if [ "x$existing_pr_count" != x0 ]; then
+  echo "existing update merge request already open. doing nothing."
+  exit 0
+fi
+
 function latest_nixpkgs() {
   name="$1"
   curl --head --silent --write-out "%{redirect_url}\n" --output /dev/null \
@@ -29,11 +36,17 @@ if is_current nixpkgs && is_current nixos; then
   exit 0
 fi
 
-echo "latest nixpkgs doesn't match my version. updating flake inputs..."
+echo "latest nixpkgs doesn't match my version. updating pinned sources..."
 npins update
 git config user.email "gitlab@matt.mattmoriarity.com"
 git config user.name "GitLab Automation"
 git add npins/sources.json
 git commit -m "npins update"
-git remote add gitlab "https://ci:$FLAKE_UPDATE_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git" || true
-git push gitlab HEAD:main
+git remote add gitlab "https://ci:$PINS_UPDATE_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git" || true
+git push -f gitlab HEAD:npins-update
+
+echo "creating merge request..."
+curl "$CI_API_V4_URL/projects/$CI_PROJECT_ID/merge_requests" \
+  -X POST \
+  -H "Authorization: Bearer $PINS_UPDATE_TOKEN" \
+  -d "{\"source_branch\":\"npins-update\",\"target_branch\":\"main\",\"title\":\"npins update\",\"assignee_id\":2}"
