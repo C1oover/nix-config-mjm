c44fd16992011ce0c4b7c4147c2c41801c576732
Matt Moriarity <matt@mattmoriarity.com>
2024-05-11 23:09:26 -0600

    remove bash scripts from tofu-scripts

--- terraform/scripts/functions.sh
+++
@@ -1,7 +0,0 @@
-link_tf_config() {
-  ln -sf "$TF_CONFIG" terraform/config.tf.json
-}
-
-tofu() {
-  (cd terraform && command tofu "$@")
-}
--- terraform/scripts/tf-apply.sh
+++
@@ -1,9 +0,0 @@
-set -o errexit
-set -o nounset
-set -o pipefail
-
-source functions.sh
-
-link_tf_config
-tofu init
-tofu apply "$@"
