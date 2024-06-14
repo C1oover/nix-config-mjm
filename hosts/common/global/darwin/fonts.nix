c825f02daf28cbf9154f9a92e5f8971a0c8ec5bf
Matt Moriarity <matt@slab.com>
2024-06-14 12:37:18 -0600

    darwin: update fonts config

--- hosts/common/global/darwin/fonts.nix
+++ hosts/common/global/darwin/fonts.nix
@@ -3,16 +3,13 @@ let
   inherit (import ../../../../packages { inherit pkgs; }) pragmata-pro;
 in
 {
-  fonts = {
-    fontDir.enable = true;
-    fonts = with pkgs; [
-      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-      pragmata-pro
-      cascadia-code
-      ibm-plex
-      iosevka
-      agave
-      (input-fonts.override { acceptLicense = true; })
-    ];
-  };
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+    pragmata-pro
+    cascadia-code
+    ibm-plex
+    iosevka
+    agave
+    (input-fonts.override { acceptLicense = true; })
+  ];
 }
