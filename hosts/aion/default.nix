be561e1cf5e48c58dbb49449498c4c05f81e329a
Matt Moriarity <matt@mattmoriarity.com>
2024-06-15 14:34:53 -0600

    aion: fix deprecated networkd config

--- hosts/aion/default.nix
+++ hosts/aion/default.nix
@@ -24,7 +24,7 @@
       matchConfig.Name = "enp1s0";
       networkConfig.DHCP = "ipv4";
       address = [ "2a01:4ff:1f0:879b::1/64" ];
-      routes = [ { routeConfig.Gateway = "fe80::1"; } ];
+      routes = [ { Gateway = "fe80::1"; } ];
     };
   };
 
