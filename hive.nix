f0c33e77fb8f0ab02a2d4be80a811a99c02883eb
Matt Moriarity <matt@mattmoriarity.com>
2024-06-25 20:52:40 -0600

    games: add patch for xivlauncher 1.0.9

--- hive.nix
+++ hive.nix
@@ -16,6 +16,11 @@ let
           url = "https://github.com/NixOS/nixpkgs/pull/309084.diff";
           hash = "sha256-3yfi8XuoM7EB8OpiwNxAi10KTf1o50Yakv1NylCu7cs=";
         })
+        (fetchpatch {
+          # xivlauncher 1.0.9 (for dawntrail)
+          url = "https://github.com/NixOS/nixpkgs/pull/323006.diff";
+          hash = "sha256-nhatCDxBamdFlEPUBf0IC5Nv27FA1JA2d/V/CVQSHNY=";
+        })
       ];
     };
 in
