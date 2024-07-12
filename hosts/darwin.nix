67ed4b06f970afd4459c0f0cc6d29dfebc3418e4
Matt Moriarity <matt@slab.com>
2024-07-12 11:15:52 -0600

    darwin: apply patch to fix swift build

--- hosts/darwin.nix
+++ hosts/darwin.nix
@@ -1,6 +1,10 @@
 let
   inputs = import ../npins;
   lib = import "${inputs.nixpkgs}/lib";
+
+  pkgsForPatching = import inputs.nixpkgs { };
+  inherit (pkgsForPatching) applyPatches fetchpatch;
+
   evalConfig = import "${inputs.darwin}/eval-config.nix";
   mkDarwin =
     arch: modules:
@@ -9,7 +13,18 @@ let
       modules = modules ++ [
         {
           nixpkgs.system = "${arch}-darwin";
-          nixpkgs.source = inputs.nixpkgs;
+          # nixpkgs.source = inputs.nixpkgs;
+          nixpkgs.source = applyPatches {
+            name = "nixpkgs-patched";
+            src = inputs.nixpkgs;
+            patches = [
+              (fetchpatch {
+                # fix swift
+                url = "https://github.com/NixOS/nixpkgs/pull/326588.diff";
+                hash = "sha256-Q0VOAEekgfOaNLd5GpC0dCsU4BXRanEwGYN2Ye5r0uc=";
+              })
+            ];
+          };
           system.checks.verifyNixPath = false;
         }
       ];
