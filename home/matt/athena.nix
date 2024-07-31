bc8feaa943199d1fba726d19ae895492fa3f477e
Matt Moriarity <matt@slab.com>
2024-07-31 12:25:58 -0600

    athena: add ,aero command for switching aerospace layouts

--- home/matt/athena.nix
+++ home/matt/athena.nix
@@ -106,4 +106,32 @@ in
       }
     ];
   };
+
+  home.packages = [
+    (pkgs.writers.writeNuBin ",aero" ''
+      let $kitty = 'net.kovidgoyal.kitty'
+      let $slack = 'com.tinyspeck.slackmacgap'
+
+      def get-window-id-for-app [app_id: string] {
+        aerospace list-windows --monitor all --app-bundle-id $app_id --format %{window-id} | split row "\n" | get 0
+      }
+
+      def set-app-layout [app_id: string, layout: string] {
+        aerospace focus --window-id (get-window-id-for-app $app_id)
+        aerospace layout $layout
+      }
+
+      def "main portable" [] {
+        set-app-layout $kitty h_accordion
+        set-app-layout $slack v_accordion
+      }
+
+      def "main docked" [] {
+        set-app-layout $kitty h_tiles
+        set-app-layout $slack v_tiles
+      }
+
+      def main [] {}
+    '')
+  ];
 }
