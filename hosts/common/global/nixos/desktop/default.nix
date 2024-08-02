402299e3ddaf93d80150c252cd1219ebecbd8b13
Matt Moriarity <matt@mattmoriarity.com>
2024-08-01 22:44:24 -0600

    desktop: always use wayland for sddm, and disable xserver

--- hosts/common/global/nixos/desktop/default.nix
+++ hosts/common/global/nixos/desktop/default.nix
@@ -16,8 +16,8 @@ in
   };
 
   config = mkIf cfg.enable {
-    services.xserver.enable = true;
     services.displayManager.sddm.enable = true;
+    services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
 
     services.dbus.packages = [ pkgs.kdePackages.kpmcore ];
--- hosts/persephone/default.nix
+++ hosts/persephone/default.nix
@@ -87,8 +87,6 @@ in
 
   networking.hostName = "persephone";
 
-  services.displayManager.sddm.wayland.enable = true;
-
   # sddm will silently wait 30 sec for a fingerprint after login before timing out
   # i don't want to login with fingerprint anyway (since it wouldn't unlock kwallet)
   security.pam.services.login.fprintAuth = false;
