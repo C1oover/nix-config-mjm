550ff1d3a70a17cd2ed5ad5964b74131253cdedb
Matt Moriarity <matt@mattmoriarity.com>
2024-09-08 18:24:17 -0600

    git: fix ,jpb command to set correct branch

--- home/matt/features/git/default.nix
+++ home/matt/features/git/default.nix
@@ -149,7 +149,7 @@ in
 
     def ,jpb [] {
       let branch = jj log -r '::@ & branches()' --no-graph -T local_branches -n 1
-      jj branch set main -r @-
+      jj branch set $branch -r @-
       try {
         jj git push
       } catch {
