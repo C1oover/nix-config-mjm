c6747ac3ee6c6c12de3da711d553219cf796436a
Matt Moriarity <matt@slab.com>
2024-09-20 10:08:55 -0600

    darwin: remove HM patch for firefox

--- hosts/common/global/darwin/default.nix
+++ hosts/common/global/darwin/default.nix
@@ -1,23 +1,7 @@
 { inputs, ... }:
-let
-  pkgsForPatching = import inputs.nixpkgs { };
-  inherit (pkgsForPatching) applyPatches fetchpatch;
-
-  home-manager-patched = applyPatches {
-    name = "home-manager-patched";
-    src = inputs.home-manager;
-    patches = [
-      (fetchpatch {
-        # fix generated profiles.ini on darwin
-        url = "https://patch-diff.githubusercontent.com/raw/nix-community/home-manager/pull/5723.diff";
-        hash = "sha256-eD2gKScImfDyF9dTQ6rqK7lf/xbucHSxthRoSBre2dY=";
-      })
-    ];
-  };
-in
 {
   imports = [
-    "${home-manager-patched}/nix-darwin"
+    "${inputs.home-manager}/nix-darwin"
     "${inputs.agenix}/modules/age.nix"
 
     ../../../../modules/nixos/nushell.nix
