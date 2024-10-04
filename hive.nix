5e664bcc7e29dabd46b5869b49a2bd210a8fd876
Matt Moriarity <matt@mattmoriarity.com>
2024-10-03 18:43:54 -0600

    persephone: use bcachefs-fstab-generator

--- hive.nix
+++ hive.nix
@@ -2,31 +2,33 @@ let
   inputs = import ./npins;
   lib = import "${inputs.nixpkgs}/lib";
 
+  pkgsForPatching = import inputs.nixos { };
+  inherit (pkgsForPatching) applyPatches fetchpatch;
+
+  patchNixpkgs =
+    {
+      src,
+      patches ? [ ],
+    }:
+    applyPatches {
+      name = "${src.name}-patched";
+      inherit src patches;
+    };
 in
-# pkgsForPatching = import inputs.nixos { };
-# inherit (pkgsForPatching) applyPatches fetchpatch;
-#
-# patchNixpkgs =
-#   {
-#     src,
-#     patches ? [ ],
-#   }:
-#   applyPatches {
-#     name = "${src.name}-patched";
-#     inherit src;
-#     patches = [
-#       (fetchpatch {
-#         url = "https://github.com/NixOS/nixpkgs/pull/343744.diff";
-#         hash = "sha256-rhbp64cHXmlxz9exElw+NEDah9/oHWBHdp8g4ImGysc=";
-#       })
-#     ] ++ patches;
-#   };
 {
   meta = {
     nixpkgs = inputs.nixos-small;
     nodeNixpkgs = {
       uranus = inputs.nixos-plasma-beta;
-      persephone = inputs.nixos;
+      persephone = patchNixpkgs {
+        src = inputs.nixos;
+        patches = [
+          (fetchpatch {
+            url = "https://github.com/NixOS/nixpkgs/pull/345207.diff";
+            hash = "sha256-VE1iEmnYDJAGl+xp5S0dpTepKnKp/wrT5cA5lNW806E=";
+          })
+        ];
+      };
     };
 
     specialArgs = {
--- hosts/persephone/default.nix
+++ hosts/persephone/default.nix
@@ -68,10 +68,16 @@ in
     pkiBundle = "/etc/secureboot";
   };
 
-  boot.initrd.clevis = {
-    enable = true;
-    devices."/dev/disk/by-partlabel/persist".secretFile = "${./persist.jwe}";
-  };
+  boot.initrd.systemd.contents."/etc/credstore.encrypted/bcachefs-sysroot-persist.mount".text = ''
+    DHzAexF2RZGcSwvqCLwg/iAAAAABAAAADAAAABAAAADeKOPpfopRrmyft1oAAAAAgAAAAAAAAAALACM
+    A8AAAACAAAAAAngAgd8Rwo7dlii5r64edJdjGHiVFfDn6FaaQCkh4WooBdxYAEI/+gJ8+cDbcuibjM7
+    edJfm67CRhyQrN7SJhvryYnekP0NiyOnyYcup8ZegedwyVjPqGsBKLdMNIlN4r0IhRioAzl/12DtTRH
+    W6FkDd8BoCG7k+lUoEbpUtVJfb4L728J3iw/GV+oqkYXJuPQ2Fcj5paMy+4SRi5LVjiAE4ACAALAAAE
+    EgAg+1290dntdlxvKWYpUns6QT/OVwfYkkEDV6lleVTWlKkAEAAglJMt0RNCJpUkWFs7G4Z8ob2/488
+    5BSkly667p8fLdKb7Xb3R2e12XG8pZilSezpBP85XB9iSQQNXqWV5VNaUqQAAAACXxBiQiqAfKycEKM
+    1aR3MGeXdUopZYMq/O7UD1QweqeNePHO4CiaB9Uwdn1+ey8xUUCmFnwHHal5sgDryt/BfB2fna8K0YS
+    BC5qn4eyZb0vNWLJvX7cHuCEq9DbjEOQN4e
+  '';
 
   boot.extraModulePackages = [ config.boot.kernelPackages.framework-laptop-kmod ];
 
