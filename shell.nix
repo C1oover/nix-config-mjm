8b1cd88ab16f1060653d4525efd6ede892ba6c07
Matt Moriarity <matt@slab.com>
2024-10-21 11:54:42 -0600

    convert top-level shell to devshell

--- shell.nix
+++ shell.nix
@@ -1,22 +1,32 @@
 let
   inputs = import ./npins;
   pkgs = import inputs.nixos { config.allowUnfree = true; };
+  devshell = import inputs.devshell { nixpkgs = pkgs; };
 in
-pkgs.mkShell {
-  packages = builtins.attrValues {
-    inherit (pkgs)
-      colmena
-      just
-      npins
-      terraform-ls
-      vault-bin
-      ;
+devshell.mkShell (
+  { lib, pkgs, ... }:
+  let
+    inherit (lib) nameValuePair;
+  in
+  {
+    commands = [
+      { package = pkgs.colmena; }
+      { package = pkgs.just; }
+      { package = pkgs.npins; }
+    ];
 
-    inherit (import ./terraform { inherit pkgs; }) opentofu;
-  };
+    devshell.packages = builtins.attrValues {
+      inherit (pkgs)
+        terraform-ls
+        vault-bin
+        ;
 
-  env = {
-    CONSUL_HTTP_ADDR = "consul.service.consul:8500";
-    VAULT_ADDR = "http://vault.service.consul:8200";
-  };
-}
+      inherit (import ./terraform { inherit pkgs; }) opentofu;
+    };
+
+    env = [
+      (nameValuePair "CONSUL_HTTP_ADDR" "consul.service.consul:8500")
+      (nameValuePair "VAULT_ADDR" "http://vault.service.consul:8200")
+    ];
+  }
+)
