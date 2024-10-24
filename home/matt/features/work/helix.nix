a2dcc0c821b8c3573f196e330595ff2f56db5b75
Matt Moriarity <matt@slab.com>
2024-10-24 08:10:00 -0600

    work: bump elixir-ls to 0.24.1

--- home/matt/features/work/helix.nix
+++ home/matt/features/work/helix.nix
@@ -61,10 +61,10 @@ in
           # use an official elixir-ls release so that it just runs with whatever elixir version
           # is in the environment. since we use asdf for the version, we can't ensure the elixir
           # version in nixpkgs matches.
-          version = "0.23.0";
+          version = "0.24.1";
           elixir-ls = pkgs.fetchzip {
             url = "https://github.com/elixir-lsp/elixir-ls/releases/download/v${version}/elixir-ls-v${version}.zip";
-            hash = "sha256-bwYV2mgxgifZVX0qY2cl/gM/sWPCAGCrO3C/eKoTYV8=";
+            hash = "sha256-68guwWL+oq3iXTxjqmNO0Bst29nbNH9xwdsuu8oUFz4=";
             stripRoot = false;
           };
         in
