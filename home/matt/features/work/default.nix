b647859cb093e4bea6c8f73603348e9d967641ce
Matt Moriarity <matt@slab.com>
2024-10-31 09:47:54 -0600

    work: remove old aliases

--- home/matt/features/work/default.nix
+++ home/matt/features/work/default.nix
@@ -72,15 +72,7 @@ in
       inherit (pkgs) cloudflared google-cloud-sdk;
     };
 
-    home.shellAliases = {
-      slab-restart = "npm run docker:down && slab-up && npm run docker:logs -- --no-log-prefix";
-      slab-up = "docker compose up -d";
-      slab-ssh = "npm run docker:ssh";
-      piex = "slab-ssh bin/phx-iex";
-    };
     programs.nushell.extraConfig = ''
-      alias piex = ,slab ssh bin/phx-iex
-
       def ",t all" [] {
         docker compose exec slab_1 mix test.all
       }
