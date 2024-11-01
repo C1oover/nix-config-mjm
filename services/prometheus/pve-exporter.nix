357812a01a416780c228768641564b0614d33a25
Matt Moriarity <matt@slab.com>
2024-11-01 10:24:38 -0600

    remove proxmoxer overlay

--- services/prometheus/pve-exporter.nix
+++ services/prometheus/pve-exporter.nix
@@ -5,20 +5,6 @@ let
 in
 {
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [
-      (final: prev: {
-        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-          (pythonFinal: pythonPrev: {
-            proxmoxer = pythonPrev.proxmoxer.overridePythonAttrs (oldAttrs: {
-              nativeCheckInputs = oldAttrs.nativeCheckInputs ++ [
-                pythonFinal.pynacl
-              ];
-            });
-          })
-        ];
-      })
-    ];
-
     services.prometheus.exporters.pve = {
       enable = true;
       environmentFile = config.vault-secrets.templates.pve-env.path;
