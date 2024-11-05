64c0bd6207ce826511b6915e999246a5c1256ee5
Matt Moriarity <matt@mattmoriarity.com>
2024-11-04 22:18:58 -0700

    add patch to fix less

--- hosts/patches.nix
+++ hosts/patches.nix
@@ -5,4 +5,6 @@
   desktops."345207".hash = "sha256-fSuTIECQKnQ0ntgcIm9zRD64EvhcU+8bLJ8kOlh7QsQ=";
   # fix 7zz build
   desktops."353272".hash = "sha256-I56z4pGA0dZ482E+CAc3kLE6v5aMSexUKBkSIZee3mI=";
+  # fix less
+  desktops."352298".hash = "sha256-skk+ldAKK+zthgNpXNj6eF59P1uzsFPqKaSNIIxZIlg=";
 }
