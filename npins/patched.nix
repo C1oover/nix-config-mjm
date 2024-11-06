9ef357cd455bad5373dd8e4e919d017f04d898e0
Matt Moriarity <matt@mattmoriarity.com>
2024-11-05 20:10:21 -0700

    remove 7zz patch

--- npins/patched.nix
+++ npins/patched.nix
@@ -54,8 +54,6 @@ overlayPatches {
   servers."352840".hash = "sha256-O4zxn4AgUrggCkqUEkmVsUAbMTL/Jhki6Q+j1mhHWLw=";
   # bcachefs-fstab-generator
   desktops."345207".hash = "sha256-fSuTIECQKnQ0ntgcIm9zRD64EvhcU+8bLJ8kOlh7QsQ=";
-  # fix 7zz build
-  desktops."353272".hash = "sha256-I56z4pGA0dZ482E+CAc3kLE6v5aMSexUKBkSIZee3mI=";
   # fix vte build on darwin
   darwin."353204".hash = "sha256-dAOW9qErQE0yLhoL9iifhXv8v4v4UWs6FnF9R8Ap4ZY=";
 }
