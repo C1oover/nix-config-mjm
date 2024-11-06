4e86b7365c0d4bbe3f5e5f444d5a76e85a23b0ec
Matt Moriarity <matt@mattmoriarity.com>
2024-11-06 08:56:30 -0700

    remove lldap patch

--- npins/patched.nix
+++ npins/patched.nix
@@ -50,8 +50,6 @@ let
       };
 in
 overlayPatches {
-  # fix lldap
-  servers."352840".hash = "sha256-O4zxn4AgUrggCkqUEkmVsUAbMTL/Jhki6Q+j1mhHWLw=";
   # bcachefs-fstab-generator
   desktops."345207".hash = "sha256-fSuTIECQKnQ0ntgcIm9zRD64EvhcU+8bLJ8kOlh7QsQ=";
   # fix vte build on darwin
