1ff524447d9fba65be4f11eb230488576388c1fb
Matt Moriarity <matt@slab.com>
2024-11-07 15:19:27 -0700

    add cmd to most aerospace shortcuts

--- home/matt/athena.nix
+++ home/matt/athena.nix
@@ -56,51 +56,51 @@ in
   xdg.configFile."aerospace/aerospace.toml".source = tomlFormat.generate "aerospace.toml" {
     start-at-login = true;
     mode.main.binding = {
-      alt-shift-slash = "layout tiles";
-      alt-shift-quote = "layout accordion";
-      alt-shift-t = "layout horizontal vertical";
+      cmd-alt-shift-slash = "layout tiles";
+      cmd-alt-shift-quote = "layout accordion";
+      cmd-alt-shift-t = "layout horizontal vertical";
 
-      alt-h = "focus left";
-      alt-j = "focus down";
-      alt-k = "focus up";
-      alt-l = "focus right";
+      cmd-alt-h = "focus left";
+      cmd-alt-j = "focus down";
+      cmd-alt-k = "focus up";
+      cmd-alt-l = "focus right";
 
-      alt-shift-h = "move left";
-      alt-shift-j = "move down";
-      alt-shift-k = "move up";
-      alt-shift-l = "move right";
+      cmd-alt-shift-h = "move left";
+      cmd-alt-shift-j = "move down";
+      cmd-alt-shift-k = "move up";
+      cmd-alt-shift-l = "move right";
 
       alt-ctrl-h = "join-with left";
       alt-ctrl-j = "join-with down";
       alt-ctrl-k = "join-with up";
       alt-ctrl-l = "join-with right";
 
-      alt-1 = "workspace 1";
-      alt-2 = "workspace 2";
-      alt-3 = "workspace 3";
-      alt-4 = "workspace 4";
-      alt-5 = "workspace 5";
-      alt-6 = "workspace 6";
-      alt-7 = "workspace 7";
-      alt-8 = "workspace 8";
-      alt-9 = "workspace 9";
+      cmd-alt-1 = "workspace 1";
+      cmd-alt-2 = "workspace 2";
+      cmd-alt-3 = "workspace 3";
+      cmd-alt-4 = "workspace 4";
+      cmd-alt-5 = "workspace 5";
+      cmd-alt-6 = "workspace 6";
+      cmd-alt-7 = "workspace 7";
+      cmd-alt-8 = "workspace 8";
+      cmd-alt-9 = "workspace 9";
 
-      alt-shift-1 = "move-node-to-workspace 1";
-      alt-shift-2 = "move-node-to-workspace 2";
-      alt-shift-3 = "move-node-to-workspace 3";
-      alt-shift-4 = "move-node-to-workspace 4";
-      alt-shift-5 = "move-node-to-workspace 5";
-      alt-shift-6 = "move-node-to-workspace 6";
-      alt-shift-7 = "move-node-to-workspace 7";
-      alt-shift-8 = "move-node-to-workspace 8";
-      alt-shift-9 = "move-node-to-workspace 9";
+      cmd-alt-shift-1 = "move-node-to-workspace 1";
+      cmd-alt-shift-2 = "move-node-to-workspace 2";
+      cmd-alt-shift-3 = "move-node-to-workspace 3";
+      cmd-alt-shift-4 = "move-node-to-workspace 4";
+      cmd-alt-shift-5 = "move-node-to-workspace 5";
+      cmd-alt-shift-6 = "move-node-to-workspace 6";
+      cmd-alt-shift-7 = "move-node-to-workspace 7";
+      cmd-alt-shift-8 = "move-node-to-workspace 8";
+      cmd-alt-shift-9 = "move-node-to-workspace 9";
 
-      alt-tab = "workspace-back-and-forth";
-      alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+      cmd-alt-tab = "workspace-back-and-forth";
+      cmd-alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
 
-      alt-shift-c = "reload-config";
+      cmd-alt-shift-c = "reload-config";
 
-      alt-shift-f = "layout floating tiling";
+      cmd-alt-shift-f = "layout floating tiling";
     };
     workspace-to-monitor-force-assignment = {
       "1" = "main";
