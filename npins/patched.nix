42f88f68076ccdaf016953824a281386290017dc
Matt Moriarity <matt@slab.com>
2024-11-12 08:00:12 -0700

    remove zellij patch

--- npins/patched.nix
+++ npins/patched.nix
@@ -53,9 +53,6 @@ overlayPatches {
   # invidious update to fix subscriptions/channels
   servers."355279".hash = "sha256-uN4FXoe7d6WU6habECrYlAi+NqweFNsiB6xkBPn0HGM=";
 
-  # zellij 0.41.1
-  darwin."353630".hash = "sha256-AAmWSZU5nMriyndtPs9xugJE7nVn0DlYdfdtvsqL9o8=";
-
   # bcachefs-fstab-generator
   desktops."345207".hash = "sha256-fSuTIECQKnQ0ntgcIm9zRD64EvhcU+8bLJ8kOlh7QsQ=";
 
