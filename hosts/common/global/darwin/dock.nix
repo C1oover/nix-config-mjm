7901e94571fbe48281c60f90b8789e9e5e755980
Matt Moriarity <matt@slab.com>
2024-12-02 11:14:11 -0700

    darwin: move dock to bottom

--- hosts/common/global/darwin/dock.nix
+++ hosts/common/global/darwin/dock.nix
@@ -3,9 +3,9 @@
     autohide = true;
     mineffect = "scale";
     mru-spaces = false;
-    orientation = "right";
+    orientation = "bottom";
     show-recents = false;
-    tilesize = 48;
+    tilesize = 72;
 
     # disable quick note
     wvous-br-corner = 1;
