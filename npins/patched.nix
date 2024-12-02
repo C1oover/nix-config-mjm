bd576d87ab8371f8802ffba741adfc7ce3c7fcfe
Matt Moriarity <matt@slab.com>
2024-12-02 08:12:35 -0700

    remove opentofu mkProvider patch for darwin

--- npins/patched.nix
+++ npins/patched.nix
@@ -58,7 +58,8 @@ let
 in
 overlayPatches {
   # opentofu: fix mkProvider
-  global."360647".hash = "sha256-OIbrP0WybgD+JWsnaUq953zo7iKxJzvkOBlL83bhFcQ=";
+  servers."360647".hash = "sha256-OIbrP0WybgD+JWsnaUq953zo7iKxJzvkOBlL83bhFcQ=";
+  desktops."360647".hash = "sha256-OIbrP0WybgD+JWsnaUq953zo7iKxJzvkOBlL83bhFcQ=";
 
   # bcachefs-fstab-generator
   desktops."345207".hash = "sha256-tI+wlhyC2e63NirN2kgg1I4GMPvMwFTIFFyo2nR1MDQ=";
