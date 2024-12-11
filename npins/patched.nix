3ae7abca4177a098f44d16e0a546ea197954090c
Matt Moriarity <matt@slab.com>
2024-12-11 08:10:58 -0700

    remove redis patch

--- npins/patched.nix
+++ npins/patched.nix
@@ -59,7 +59,4 @@ in
 overlayPatches {
   # bcachefs-fstab-generator
   desktops."345207".hash = "sha256-tI+wlhyC2e63NirN2kgg1I4GMPvMwFTIFFyo2nR1MDQ=";
-
-  # redis - complete lib refactor
-  servers."363775".hash = "sha256-a9U9P0nCJMva4SfRUNaPCLSvcxWtDfSai0drEuwIWCg=";
 }
