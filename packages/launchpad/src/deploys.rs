8c32f5f072e55c890f028e345d2a057b6f51f7e9
Matt Moriarity <matt@mattmoriarity.com>
2024-12-15 11:21:45 -0700

    launchpad(deploys): show more info for open mrs

--- packages/launchpad/src/deploys.rs
+++ packages/launchpad/src/deploys.rs
@@ -2,9 +2,10 @@ pub(crate) mod routes;
 
 use anyhow::Result;
 use chrono::{DateTime, Utc};
+use futures::future::try_join_all;
 use gitlab::api::common::SortOrder;
-use gitlab::api::projects::merge_requests::MergeRequestState;
 use gitlab::api::projects::{self, deployments::DeploymentOrderBy};
+use gitlab::api::projects::{merge_requests, pipelines};
 use gitlab::api::{raw, AsyncQuery};
 use gitlab::AsyncGitlab;
 use serde::{Deserialize, Serialize};
@@ -25,20 +26,40 @@ impl GitLabClient {
         GitLabClient { client }
     }
 
-    #[tracing::instrument(skip(self))]
-    pub async fn get_update_merge_request(self: &Self) -> Result<Option<MergeRequest>> {
-        let endpoint = projects::merge_requests::MergeRequests::builder()
+    #[tracing::instrument(skip(self), ret, err)]
+    pub async fn list_open_merge_requests(self: &Self) -> Result<Vec<MergeRequest>> {
+        let endpoint = merge_requests::MergeRequests::builder()
             .project(GITLAB_PROJECT)
-            .source_branch("npins-update")
             .target_branch("main")
-            .state(MergeRequestState::Opened)
+            .state(merge_requests::MergeRequestState::Opened)
+            .view(merge_requests::MergeRequestView::Simple)
             .build()?;
 
-        let mut mrs: Vec<MergeRequest> = endpoint.query_async(&self.client).await?;
-        Ok(mrs.pop())
+        let simple_mrs: Vec<MergeRequestSimple> = endpoint.query_async(&self.client).await?;
+
+        Ok(try_join_all(simple_mrs.iter().map(|mr| self.get_merge_request(mr.iid))).await?)
     }
 
-    #[tracing::instrument(skip(self), ret)]
+    #[tracing::instrument(skip(self), ret, err)]
+    async fn get_merge_request(self: &Self, iid: u64) -> Result<MergeRequest> {
+        let endpoint = merge_requests::MergeRequest::builder()
+            .project(GITLAB_PROJECT)
+            .merge_request(iid)
+            .build()?;
+
+        let mut mr: MergeRequest = endpoint.query_async(&self.client).await?;
+
+        let jobs_endpoint = pipelines::PipelineJobs::builder()
+            .project(GITLAB_PROJECT)
+            .pipeline(mr.head_pipeline.id)
+            .build()?;
+
+        mr.head_pipeline.jobs = jobs_endpoint.query_async(&self.client).await?;
+
+        Ok(mr)
+    }
+
+    #[tracing::instrument(skip(self), ret, err)]
     pub async fn list_deployments(self: &Self) -> Result<Vec<Deployment>> {
         let endpoint = projects::deployments::Deployments::builder()
             .project(GITLAB_PROJECT)
@@ -49,7 +70,7 @@ impl GitLabClient {
         Ok(endpoint.query_async(&self.client).await?)
     }
 
-    #[tracing::instrument(skip(self), ret)]
+    #[tracing::instrument(skip(self), ret, err)]
     pub async fn get_hosts_file(self: &Self) -> Result<String> {
         let endpoint = projects::repository::files::FileRaw::builder()
             .project(GITLAB_PROJECT)
@@ -61,7 +82,7 @@ impl GitLabClient {
         )?)
     }
 
-    #[tracing::instrument(skip(self))]
+    #[tracing::instrument(skip(self), err)]
     pub async fn update_hosts_file(self: &Self, content: &str) -> Result<()> {
         let endpoint = projects::repository::files::UpdateFile::builder()
             .project(GITLAB_PROJECT)
@@ -77,11 +98,37 @@ impl GitLabClient {
     }
 }
 
+#[derive(Deserialize, Debug)]
+struct MergeRequestSimple {
+    iid: u64,
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 struct MergeRequest {
     id: i64,
     iid: i64,
     title: String,
+    state: MergeRequestState,
+    web_url: String,
+    created_at: DateTime<Utc>,
+    head_pipeline: Pipeline,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+enum MergeRequestState {
+    Opened,
+    Closed,
+    Locked,
+    Merged,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+struct Pipeline {
+    id: u64,
+    iid: u64,
+    #[serde(skip)]
+    jobs: Vec<Job>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -121,7 +168,7 @@ enum DeploymentStatus {
     Unknown,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 struct Job {
     name: String,
     stage: String,
@@ -137,13 +184,15 @@ impl Job {
     fn friendly_name(self: &Self) -> String {
         match self.name.as_str() {
             "deploy nixos hosts" => "NixOS".to_string(),
+            "build nixos hosts" => "NixOS".to_string(),
             "apply terranix changes" => "Infra".to_string(),
+            "build tofu changes" => "Infra".to_string(),
             name => name.to_string(),
         }
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 enum JobStatus {
     Created,
@@ -159,7 +208,7 @@ enum JobStatus {
     Unknown,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 struct Commit {
     id: String,
     message: String,
--- packages/launchpad/src/deploys/routes.rs
+++ packages/launchpad/src/deploys/routes.rs
@@ -1,10 +1,11 @@
 use axum::{extract::State, response::IntoResponse, routing::get, Router};
-use maud::html;
+use chrono_humanize::{Accuracy, HumanTime, Tense};
+use maud::{html, Markup};
 use tokio::try_join;
 
-use crate::app;
+use crate::{app, deploys::JobStatus};
 
-use super::GitLabClient;
+use super::{GitLabClient, MergeRequest};
 
 pub fn router() -> Router<app::State> {
     Router::new().route("/deploys", get(index))
@@ -12,23 +13,24 @@ pub fn router() -> Router<app::State> {
 
 #[tracing::instrument(skip(client))]
 async fn index(State(client): State<GitLabClient>) -> Result<impl IntoResponse, app::Error> {
-    let (update_mr, deploys) =
-        try_join!(client.get_update_merge_request(), client.list_deployments())?;
+    let (mrs, deploys) = try_join!(client.list_open_merge_requests(), client.list_deployments())?;
 
     Ok(app::layout(
         "Deploys",
         html! {
-            h1 { "Deploys" }
+            h1 .mb-4 { "Deploys" }
 
-            @if let Some(mr) = update_mr {
-                p .alert .alert-primary .mt-3 role="alert" {
-                    "There is an outstanding update "
-                    a href={ "https://git.midna.dev/mjm/nix-config/-/merge_requests/" (mr.iid) } target="_blank" {
-                        "merge request"
+            @if !mrs.is_empty() {
+                h2 .mb-3 { "Open merge requests"}
+
+                ul .list-group .mb-4 {
+                    @for mr in &mrs {
+                        (merge_request_row(&mr))
                     }
-                    "."
                 }
             }
+
+            h2 .mb-3 { "Recent deployments" }
 
             ul .list-group {
                 @for deploy in &deploys {
@@ -56,4 +58,63 @@ async fn index(State(client): State<GitLabClient>) -> Result<impl IntoResponse, 
             }
         },
     ))
+}
+
+fn merge_request_row(mr: &MergeRequest) -> Markup {
+    html! {
+        li
+            .list-group-item {
+
+            a
+                href=(mr.web_url)
+                target="_blank"
+                .text-decoration-none
+                .text-reset {
+
+                div .d-flex .w-100 .justify-content-between {
+                    h5 { (mr.title) }
+                    small {
+                        "opened "
+                        (HumanTime::from(mr.created_at).to_text_en(Accuracy::Rough, Tense::Past))
+                    }
+                }
+
+                ul .list-unstyled {
+                    @for job in &mr.head_pipeline.jobs {
+                        li {
+                            @match job.status {
+                                JobStatus::Success => i .bi-check-circle-fill .text-success {},
+                                JobStatus::Failed => i .bi-x-circle-fill .text-danger {},
+                                JobStatus::Running => i .bi-record-circle .text-primary {},
+                                _ => i .bi-question-circle-fill .text-secondary {},
+                            }
+                            " "
+                            span { (job.friendly_name()) }
+                            " - "
+                            @match (job.started_at, job.finished_at) {
+                                (Some(started_at), Some(finished_at)) => {
+                                    span {
+                                        "finished "
+                                        time { (HumanTime::from(finished_at).to_text_en(Accuracy::Rough, Tense::Past)) }
+                                        " (took "
+                                        (HumanTime::from(finished_at - started_at).to_text_en(Accuracy::Rough, Tense::Present))
+                                        ")"
+                                    }
+                                },
+                                (Some(started_at), None) => {
+                                    span {
+                                        "started "
+                                        time { (HumanTime::from(started_at).to_text_en(Accuracy::Rough, Tense::Past)) }
+                                    }
+                                },
+                                (None, _) => {
+                                    span { "not yet started" }
+                                },
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
