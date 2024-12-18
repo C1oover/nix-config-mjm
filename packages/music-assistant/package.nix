0f1932761178c20884bdc12d184873f1035ad51e
Matt Moriarity <matt@slab.com>
2024-12-18 11:55:53 -0700

    music-assistant: remove dependencies patch
    nixpkgs now includes a version of this

--- packages/music-assistant/package.nix
+++ packages/music-assistant/package.nix
@@ -8,5 +8,4 @@
   preBuild = ''
     ln -sf ${cliraop}/bin/cliraop music_assistant/server/providers/airplay/bin/cliraop-linux-x86_64
   '';
-  patches = oldAttrs.patches ++ [ ./ma-no-install.patch ];
 })
