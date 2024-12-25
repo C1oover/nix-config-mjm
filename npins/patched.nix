ca169ae9fed416787cdcb5027cbe82d6610a55b9
Matt Moriarity <matt@mattmoriarity.com>
2024-12-24 17:11:54 -0700

    add patch to fix freeipmi build

--- npins/patched.nix
+++ npins/patched.nix
@@ -1,4 +1,7 @@
 import ../lib/overlay-patches.nix {
   # bcachefs-fstab-generator
   nixos."345207".hash = "sha256-tI+wlhyC2e63NirN2kgg1I4GMPvMwFTIFFyo2nR1MDQ=";
+
+  # freeipmi: fix build with GCC14
+  nixos-small."367923".hash = "sha256-tVe9MHpWDgPBbG+TU+sIjy/DwZQBy3tKzQdMZamD2NA=";
 }
