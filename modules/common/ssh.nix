c4034917863301f003c3ca53f5706c9ad6717382
Matt Moriarity <matt@mattmoriarity.com>
2025-01-02 16:44:50 -0700

    common/ssh: switch hades to use mjm as username

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -59,13 +59,13 @@ in
       CanonicalizeHostname yes
       CanonicalDomains home.mattmoriarity.com
 
-      Host artemis.home.mattmoriarity.com hades.home.mattmoriarity.com
+      Host artemis.home.mattmoriarity.com
         User root
 
       Host aion
         Hostname 5.78.46.61
 
-      Host apollo.home.mattmoriarity.com talos.home.mattmoriarity.com
+      Host apollo.home.mattmoriarity.com hades.home.mattmoriarity.com talos.home.mattmoriarity.com
         User mjm
     '';
   };
