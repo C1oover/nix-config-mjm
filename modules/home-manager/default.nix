ac5c644478b2efc15a3f6eae74349d8e3a8198ec
Matt Moriarity <matt@mattmoriarity.com>
2025-01-02 20:33:22 -0700

    home-manager: move features to modules dir

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,21 +3,19 @@
   imports = [
     "${inputs.catppuccin}/modules/home-manager"
 
+    ./aerospace
     ./base.nix
+    ./desktop
     ./dock.nix
-
-    # TODO move these to this directory
-    ../../home/matt/features/aerospace
-    ../../home/matt/features/desktop
-    ../../home/matt/features/emacs
-    ../../home/matt/features/email
-    ../../home/matt/features/firefox
-    ../../home/matt/features/git
-    ../../home/matt/features/helix
-    ../../home/matt/features/homelab
-    ../../home/matt/features/shell
-    ../../home/matt/features/syncthing
-    ../../home/matt/features/terminal
-    ../../home/matt/features/work
+    ./emacs.nix
+    ./email.nix
+    ./firefox
+    ./git
+    ./helix
+    ./homelab
+    ./shell
+    ./syncthing.nix
+    ./terminal
+    ./work
   ];
 }
