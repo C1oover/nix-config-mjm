88974635e63ad4ff666219cd1187f94e733251f7
Matt Moriarity <matt@mattmoriarity.com>
2025-01-13 23:19:18 -0700

    nixos-deploy-old: remove

--- packages/nixos-deploy-old/lib/nixos_deploy/consul.ex
+++
@@ -1,8 +0,0 @@
-defmodule NixosDeploy.Consul do
-  def get_service_health(service, node) do
-    Req.get!("http://consul.service.consul:8500/v1/health/service/:service",
-      params: [filter: "Node.Node == \"#{node}\""],
-      path_params: [service: service]
-    ).body
-  end
-end
--- packages/nixos-deploy-old/test/nixos_deploy_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule NixosDeployTest do
-  use ExUnit.Case
-  doctest NixosDeploy
-
-  test "greets the world" do
-    assert NixosDeploy.hello() == :world
-  end
-end
