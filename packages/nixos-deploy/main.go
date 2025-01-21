f40ee85427df06a45f2e5bc9c8fbd7fc4f308ff6
Matt Moriarity <matt@mattmoriarity.com>
2025-01-20 19:57:02 -0700

    nixos-deploy: fix exit codes for errors

--- packages/nixos-deploy/main.go
+++ packages/nixos-deploy/main.go
@@ -42,21 +42,26 @@ func main() {
 	case "deploy":
 		if err := handleDeploy(ctx); err != nil {
 			slog.ErrorContext(ctx, "deploy failed", "error", err)
+			os.Exit(1)
 		}
 	case "diff":
 		if err := handleDiff(ctx); err != nil {
 			slog.ErrorContext(ctx, "diff failed", "error", err)
+			os.Exit(1)
 		}
 	case "reboot":
 		if err := handleReboot(ctx); err != nil {
 			slog.ErrorContext(ctx, "reboot failed", "error", err)
+			os.Exit(1)
 		}
 	case "apply-local":
 		if err := handleApplyLocal(ctx); err != nil {
 			slog.ErrorContext(ctx, "apply failed", "error", err)
+			os.Exit(1)
 		}
 	default:
 		slog.ErrorContext(ctx, "unexpected command", "command", flag.Arg(0))
+		os.Exit(1)
 	}
 }
 
