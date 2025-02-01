728724cd9220cca3ea5ce5036f339b4df71974b9
Matt Moriarity <matt@mattmoriarity.com>
2025-01-31 21:41:09 -0700

    hm/git: move remaining aliases into jj config

--- modules/home-manager/git/jujutsu.nix
+++ modules/home-manager/git/jujutsu.nix
@@ -21,6 +21,14 @@ let
       (lib.makeBinPath [ pkgs.jujutsu ])
     ];
   } ./scripts/jm.nu;
+
+  mkAlias = script: [
+    "util"
+    "exec"
+    "--"
+    script
+  ];
+  mkFishAlias = name: text: mkAlias (pkgs.writers.writeFish name text);
 in
 {
   options.mjm.git = {
@@ -88,73 +96,33 @@ in
             "--to"
             "@"
           ];
-          mega = [
-            "util"
-            "exec"
-            "--"
-            (getExe jm)
+          mega = mkAlias (getExe jm);
+          p = [ "git push" ];
+          pc = [
+            "git"
+            "push"
+            "--change"
+            "heads(description(glob:'?*') & ::@)"
           ];
+          pb = mkFishAlias "jj-pb" ''
+            jj bookmark move --from 'heads(::@ & bookmarks())' --to 'heads(description(glob:"?*") & ::@)'
+            jj git push
+            or jj undo
+          '';
+          up = mkFishAlias "jj-up" ''
+            jj git fetch
+            jj rebase -d 'trunk()'
+          '';
+          f = mkFishAlias "jj-f" ''
+            jj log --no-graph --color always -T 'if(description, change_id.short() ++ " " ++ description.first_line() ++ "\n")' $argv |
+              sk --nth 2.. --ansi --preview 'jj show --color always {1}' |
+              string split -f 1 ' '
+          '';
         };
         revsets = {
           log = "@ | trunk() | ancestors(trunk()..(visible_heads() & mine() & ~tags()), 2)";
         };
       };
     };
-
-    programs.fish.shellAliases = {
-      ",jp" = "jj git push";
-      ",jpc" = "jj git push -c 'heads(description(glob:\"?*\") & ::@)'";
-    };
-    programs.fish.functions = {
-      ",jpb" = ''
-        jj bookmark move --from 'heads(::@ & bookmarks())' --to 'heads(description(glob:"?*") & ::@)'
-        jj git push
-        or jj undo
-      '';
-
-      ",ju" = ''
-        jj git fetch
-        jj rebase -d 'trunk()'
-      '';
-
-      ",jf" = ''
-        jj log --no-graph --color always -T 'if(description, change_id.short() ++ " " ++ description.first_line() ++ "\n")' $argv |
-          sk --nth 2.. --ansi --preview 'jj show --color always {1}' |
-          string split -f 1 ' '
-      '';
-    };
-
-    programs.nushell.shellAliases = {
-      ",jgf" = "jj git fetch";
-      ",jp" = "jj git push";
-      ",jrm" = "jj rebase -d main";
-    };
-    programs.nushell.extraConfig = ''
-      def ,jpc [] {
-        jj git push --change 'heads(description(glob:"?*") & ::@)'
-      }
-
-      def ,jpb [] {
-        jj bookmark move --from 'heads(::@ & bookmarks())' --to 'heads(description(glob:"?*") & ::@)'
-        try {
-          jj git push
-        } catch {
-          jj undo
-        }
-      }
-
-      def ,ju [] {
-        jj git fetch
-        jj rebase -d 'trunk()'
-      }
-
-      def --wrapped ,jr [...rest] {
-        jj rebase -d (,jf) ...$rest
-      }
-
-      def --wrapped ,je [...rest] {
-        jj edit (,jf ...$rest)
-      }
-    '';
   };
 }
