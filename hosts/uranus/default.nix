6f440a87a70f578734ad27c3b62b08094f89a392
Matt Moriarity <matt@mattmoriarity.com>
2025-02-01 11:56:25 -0700

    uranus: move wireplumber config to its own module

--- hosts/uranus/default.nix
+++ hosts/uranus/default.nix
@@ -4,9 +4,12 @@
   ...
 }:
 {
-  imports = [ ./hardware-configuration.nix ];
 
   deployment.targetHost = null;
+  imports = [
+    ./hardware-configuration.nix
+    ./wireplumber.nix
+  ];
 
   environment.systemPackages = lib.attrValues {
     inherit (pkgs) chrysalis unofficial-homestuck-collection;
@@ -24,78 +27,6 @@
   services.udev.packages = [ pkgs.chrysalis ];
 
   services.openssh.enable = true;
-
-  services.pipewire.wireplumber = {
-    extraScripts."mjm/select-correct-profile.lua" = builtins.readFile ./select-correct-profile.lua;
-    extraConfig.dell-monitor = {
-      "context.properties" = {
-        "log.level" = "I";
-      };
-
-      "monitor.alsa.rules" = [
-        # prioritize the displayport audio over the yeti mic,
-        # so that we stay on the DP audio device even when switching
-        # between profiles (which changes the node name, so the
-        # remembered default node gets ignored)
-        {
-          matches = [
-            { "api.alsa.card.name" = "HDA ATI HDMI"; }
-          ];
-          actions = {
-            update-props = {
-              "priority.session" = "1200";
-            };
-          };
-        }
-
-        # disable the motherboard's audio device
-        {
-          matches = [
-            { "api.alsa.card.name" = "HD-Audio Generic"; }
-            { }
-          ];
-          actions = {
-            update-props = {
-              "device.disabled" = true;
-            };
-          };
-        }
-
-        # disable the sink node for the Yeti mic and the audio source for the webcam
-        {
-          matches = [
-            {
-              "media.class" = "Audio/Sink";
-              "api.alsa.card.name" = "Yeti Stereo Microphone";
-            }
-            {
-              "media.class" = "Audio/Source";
-              "api.alsa.card.name" = "HD Pro Webcam C920";
-            }
-          ];
-          actions = {
-            update-props = {
-              "node.disabled" = true;
-            };
-          };
-        }
-      ];
-
-      # add a custom hook before the existing profile selection
-      # logic that figures out which profile matches the main
-      # monitor, and chooses that one.
-      "wireplumber.components" = [
-        {
-          name = "mjm/select-correct-profile.lua";
-          type = "script/lua";
-          provides = "hooks.mjm.select-correct-profile";
-        }
-      ];
-      "wireplumber.profiles".main = {
-        "hooks.mjm.select-correct-profile" = "required";
-      };
-    };
-  };
 
   system.stateVersion = "24.05";
 }
