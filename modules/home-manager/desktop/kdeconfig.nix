c7d524d84076a42366339079c57be9210ecfd42c
Matt Moriarity <matt@mattmoriarity.com>
2025-02-01 08:40:56 -0700

    hm/desktop: update kde configs

--- modules/home-manager/desktop/kdeconfig.nix
+++ modules/home-manager/desktop/kdeconfig.nix
@@ -14,16 +14,11 @@ in
       kdeglobals = pkgs.writeText "kdeglobals" ''
         [General]
         AccentColor=146,110,228
-        TerminalApplication=kitty
-        TerminalService=kitty.desktop
+        TerminalApplication=ghostty
+        TerminalService=com.mitchellh.ghostty.desktop
 
         [KDE]
         LookAndFeelPackage=org.kde.breezedark.desktop
-      '';
-
-      kglobalshortcutsrc = pkgs.writeText "kglobalshortcutsrc" ''
-        [services][org.kde.krunner.desktop]
-        _launch=Alt+F2\tMeta+Space\tSearch
       '';
 
       krunnerrc = pkgs.writeText "krunnerrc" ''
@@ -32,28 +27,8 @@ in
       '';
 
       kwinrc = pkgs.writeText "kwinrc" ''
-        [Windows]
-        FocusPolicy=FocusFollowsMouse
-
         [Xwayland]
         Scale=1.25
-      '';
-
-      # not sure if this one is safe, particularly with the indices for Containments
-      # plasma-org.kde.plasma.desktop-appletsrc = pkgs.writeText "appletsrc" ''
-      #   [Containments][1][Wallpaper][org.kde.image][General]
-      #   Image=/run/current-system/sw/share/wallpapers/MilkyWay/
-      #   PreviewImage=/run/current-system/sw/share/wallpapers/MilkyWay/
-
-      #   [Containments][2][Wallpaper][org.kde.image][General]
-      #   Image=/run/current-system/sw/share/wallpapers/MilkyWay/
-      #   PreviewImage=/run/current-system/sw/share/wallpapers/MilkyWay/
-      # '';
-
-      kscreenlockerrc = pkgs.writeText "kscreenlockerrc" ''
-        [Greeter][Wallpaper][org.kde.image][General]
-        Image=/run/current-system/sw/share/wallpapers/MilkyWay/
-        PreviewImage=/run/current-system/sw/share/wallpapers/MilkyWay/
       '';
     };
 
