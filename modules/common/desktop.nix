e0baedba7d1334db92f3e0c7c72911b391befb40
Matt Moriarity <matt@slab.com>
2025-02-10 08:44:03 -0700

    hm/desktop: remove unused fonts

--- modules/common/desktop.nix
+++ modules/common/desktop.nix
@@ -15,16 +15,8 @@ in
 
   config = mkIf cfg.enable {
     fonts.packages = with pkgs; [
-      nerd-fonts.monaspace
-      nerd-fonts.symbols-only
       pragmata-pro
-      cascadia-code
-      ibm-plex
-      agave
-      (input-fonts.override { acceptLicense = true; })
-      departure-mono
       noto-fonts
-      monaspace
     ];
   };
 }
