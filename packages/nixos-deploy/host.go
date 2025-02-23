62213631f0467ed6337eb43f66de98a1c243d83b
Matt Moriarity <matt@mattmoriarity.com>
2025-02-23 08:36:11 -0700

    nixos-deploy: remove tests from hosts

--- packages/nixos-deploy/host.go
+++ packages/nixos-deploy/host.go
@@ -25,7 +25,6 @@ type Host struct {
 	OutPath      string
 	DeployConfig DeployConfig
 	RebootNeeded bool
-	Tests        []string
 	cfg          Config
 	log          *slog.Logger
 	remoteRunner cmd.Runner
@@ -73,24 +72,6 @@ func (h *Host) Build(ctx context.Context, useNom bool) error {
 	}
 
 	l.InfoContext(ctx, "finished building host", "out_path", h.OutPath)
-	return nil
-}
-
-func (h *Host) Test(ctx context.Context, useNom bool) error {
-	if len(h.Tests) == 0 {
-		return nil
-	}
-
-	h.log.InfoContext(ctx, "testing host")
-
-	for _, t := range h.Tests {
-		h.log.InfoContext(ctx, "running test", "drv_path", t)
-		if err := h.cfg.Nix.Realise(ctx, []string{t}, useNom); err != nil {
-			return fmt.Errorf("building test %s: %w", t, err)
-		}
-	}
-
-	h.log.InfoContext(ctx, "finished testing host")
 	return nil
 }
 
