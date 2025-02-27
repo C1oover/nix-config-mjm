c447697266d14174ec8006bafa073f1c2c1ffc23
Matt Moriarity <matt@mattmoriarity.com>
2025-02-26 22:35:45 -0700

    nixos-deploy: handle setting vault token

--- packages/nixos-deploy/ssh.go
+++ packages/nixos-deploy/ssh.go
@@ -8,10 +8,48 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/hashicorp/vault/api"
 	"golang.org/x/crypto/ssh"
 )
+
+func newVaultClient(ctx context.Context) (*api.Client, error) {
+	c, err := api.NewClient(nil)
+	if err != nil {
+		return nil, fmt.Errorf("creating vault client: %w", err)
+	}
+
+	idToken := os.Getenv("VAULT_ID_TOKEN")
+	if idToken == "" {
+		// if no id token for jwt auth, assume this is called from somewhere where the
+		// vault cli is being used, and try to read the token it stores
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return nil, fmt.Errorf("getting user home dir: %w", err)
+		}
+
+		tokenBytes, err := os.ReadFile(path.Join(homeDir, ".vault-token"))
+		if err != nil {
+			return nil, fmt.Errorf("reading vault token from file: %w", err)
+		}
+
+		c.SetToken(strings.TrimSpace(string(tokenBytes)))
+	} else {
+		req := map[string]any{
+			"role": "homelab-infra",
+			"jwt":  idToken,
+		}
+		resp, err := c.Logical().WriteWithContext(ctx, "auth/gitlab/login", req)
+		if err != nil {
+			return nil, fmt.Errorf("authorizing vault with jwt token: %w", err)
+		}
+
+		c.SetToken(resp.Auth.ClientToken)
+	}
+
+	return c, nil
+}
 
 func generateSSHKey(ctx context.Context) (*ssh.Certificate, crypto.PrivateKey, error) {
 	pubKey, privKey, err := ed25519.GenerateKey(nil)
@@ -26,14 +64,14 @@ func generateSSHKey(ctx context.Context) (*ssh.Certificate, crypto.PrivateKey, e
 
 	pubKeyBytes := ssh.MarshalAuthorizedKey(sshPubKey)
 
-	c, err := api.NewClient(nil)
+	c, err := newVaultClient(ctx)
 	if err != nil {
 		return nil, nil, fmt.Errorf("creating vault client: %w", err)
 	}
 
 	sshEngine := c.SSHWithMountPoint("ssh-client-signer")
 
-	secret, err := sshEngine.SignKeyWithContext(ctx, "homelab-client", map[string]interface{}{
+	secret, err := sshEngine.SignKeyWithContext(ctx, "homelab-client", map[string]any{
 		"public_key":       string(pubKeyBytes),
 		"valid_principals": "matt,mjm",
 	})
