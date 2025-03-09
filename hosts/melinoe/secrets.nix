16ff43ef2ae9d81befb32c04b778d97df2c20609
Matt Moriarity <matt@mattmoriarity.com>
2025-03-08 21:18:35 -0700

    melinoe: migrate to libvirtd

--- hosts/melinoe/secrets.nix
+++ hosts/melinoe/secrets.nix
@@ -1,13 +1,21 @@
 {
   vault-secrets.roleId = "a1b1c0f2-15de-9664-7729-ee482397d981";
+
+  # proxmox vm
+  # vault-secrets.encryptedSecretId = ''
+  #   DHzAexF2RZGcSwvqCLwg/iAAAAABAAAADAAAABAAAACXbCU98TWqCN0u3tsAAAAAgAAAAAAAAAALACM
+  #   A8AAAACAAAAAAngAgMRIL2fOnQAL4HoThT/PNgFz6jsALiJg2TJ5IX10tq8wAEKwBcQMRvrqqSMjwYy
+  #   brXN4+VIyhOmT6YzrjpxHIF+BOOtl6Av9ojlJyhCjGdl6p/XFXdASuRvjaM+2SHuQOhRsj3r4QI1HEg
+  #   oaP1ehVnuRRONQP/bWfI0qk/QZeJ/b93CRuBv3alqrGaKiAtU0qRf8YiYMjn3Ec/zc8AE4ACAALAAAE
+  #   EgAg/928wzzwZljax6QULbex0pLe130yDZJRVcq/rrtdgRsAEAAg/leG+hSsL2gBiUXHSSzj3HI0jkk
+  #   Dbh7UxWJgx2Cef+z/3bzDPPBmWNrHpBQtt7HSkt7XfTINklFVyr+uu12BGwAAAACz9FgLGumnFkuMlo
+  #   E2VTbZm4PVL+BP4igOrLoGDHWfT0axkAoRbSeddGccm6D3GrnFHSdpYkRwLjSO8tfa52y92xYIEXumk
+  #   +LyXVCc6qHywCM0Suw=
+  # '';
+
   vault-secrets.encryptedSecretId = ''
-    DHzAexF2RZGcSwvqCLwg/iAAAAABAAAADAAAABAAAACXbCU98TWqCN0u3tsAAAAAgAAAAAAAAAALACM
-    A8AAAACAAAAAAngAgMRIL2fOnQAL4HoThT/PNgFz6jsALiJg2TJ5IX10tq8wAEKwBcQMRvrqqSMjwYy
-    brXN4+VIyhOmT6YzrjpxHIF+BOOtl6Av9ojlJyhCjGdl6p/XFXdASuRvjaM+2SHuQOhRsj3r4QI1HEg
-    oaP1ehVnuRRONQP/bWfI0qk/QZeJ/b93CRuBv3alqrGaKiAtU0qRf8YiYMjn3Ec/zc8AE4ACAALAAAE
-    EgAg/928wzzwZljax6QULbex0pLe130yDZJRVcq/rrtdgRsAEAAg/leG+hSsL2gBiUXHSSzj3HI0jkk
-    Dbh7UxWJgx2Cef+z/3bzDPPBmWNrHpBQtt7HSkt7XfTINklFVyr+uu12BGwAAAACz9FgLGumnFkuMlo
-    E2VTbZm4PVL+BP4igOrLoGDHWfT0axkAoRbSeddGccm6D3GrnFHSdpYkRwLjSO8tfa52y92xYIEXumk
-    +LyXVCc6qHywCM0Suw=
+    Whxqht+dQJax1aZeCGLxmiAAAAABAAAADAAAABAAAACsC6sfGU56JQCa9BUAAAAAQiG7g
+    RokH3Rl8C32UKtIsO3mxQbwFZBp6mx7Xcjbm6uICTFlaZdb5kW4CeK5dI/Fo66SKY3j0j
+    hGhJsz8rsGOFKt2qyleydz/TMAtx3ERycH67J9
   '';
 }
