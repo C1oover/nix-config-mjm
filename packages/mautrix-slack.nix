14ade0d4f4fb1a953ec863362973a5b14aca631a
Matt Moriarity <matt@mattmoriarity.com>
2025-03-16 18:01:01 -0600

    mautrix-slack: 0.1.4 -> 0.2.0

--- packages/mautrix-slack.nix
+++ packages/mautrix-slack.nix
@@ -6,19 +6,19 @@
 }:
 
 buildGoModule rec {
-  name = "mautrix-slack";
-  version = "0.1.4";
+  pname = "mautrix-slack";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "mautrix";
     repo = "slack";
     rev = "v${version}";
-    hash = "sha256-MDbWvbEY8+CrUL1SnjdJ4SqyOH/5gPsEQkLnTHyJdOo=";
+    hash = "sha256-y6DsojQQAQkAB4puhk3DLheVvjn0puoYvzoX1H7gBEM=";
   };
 
   buildInputs = [ olm ];
 
-  vendorHash = "sha256-8U6ifMLRF7PJyG3hWKgBtj/noO/eCXXD60aeB4p2W54=";
+  vendorHash = "sha256-1aYg6wDG2hzUshtHs9KiJDAFb4OM1oupUJAh3LR4KxY=";
 
   meta = with lib; {
     description = " A Matrix-Slack puppeting bridge";
