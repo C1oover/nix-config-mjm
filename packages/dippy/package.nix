381cec9876210c09a26491d3d5c2cfb9f48c7170
Matt Moriarity <matt@mattmoriarity.com>
2025-03-29 10:44:01 -0600

    Back out "dippy: use lix-eval-jobs"
    This backs out commit f40fd2cb1ac6e5a652abdb69613aa074dce3d94b.

--- packages/dippy/package.nix
+++ packages/dippy/package.nix
@@ -3,8 +3,6 @@
   buildGoModule,
   attic-client,
   makeWrapper,
-  fetchzip,
-  lix,
   nix-eval-jobs,
   nix-output-monitor,
   nvd-json,
@@ -14,17 +12,6 @@
 
 let
   pulumiGo = pulumi.withPackages (p: [ p.pulumi-language-go ]);
-  lix-eval-jobs = (nix-eval-jobs.override { nix = lix; }).overrideAttrs (oldAttrs: {
-    src = fetchzip {
-      url = "https://git.lix.systems/api/v1/repos/lix-project/nix-eval-jobs/archive/f8869bdcca7c1d5aaf37de3da3a4176811279a57.tar.gz?rev=f8869bdcca7c1d5aaf37de3da3a4176811279a57";
-      hash = "sha256-F/RvI9chHywnckEqHO1ggjzCayknhDnnl2kNnnVXpWg=";
-    };
-    version = "2.91.0-lix-f8869bd";
-
-    mesonBuildType = "debugoptimized";
-
-    ninjaFlags = oldAttrs.ninjaFlags or [ ] ++ [ "-v" ];
-  });
 in
 
 buildGoModule {
@@ -43,7 +30,7 @@ buildGoModule {
     wrapProgram $out/bin/dippy \
       --prefix PATH : ${
         lib.makeBinPath [
-          lix-eval-jobs
+          nix-eval-jobs
           nix-output-monitor
           nvd-json
           nvd
