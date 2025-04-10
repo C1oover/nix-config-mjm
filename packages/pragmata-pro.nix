9069178be1f0de2ac64d5aec199bf5e40fb516c2
Matt Moriarity <matt@slab.com>
2025-04-10 10:00:35 -0600

    pragmata-pro: 0.830 -> 0.9

--- packages/pragmata-pro.nix
+++ packages/pragmata-pro.nix
@@ -6,12 +6,14 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "pragmata-pro";
-  version = "0.830";
+  version = "0.9";
 
   src = requireFile {
-    name = "PP-ep6wq.zip";
+    name = "PragmataPro0.9-8svlok.zip";
     url = "https://fsd.it/shop/fonts/pragmatapro/";
-    sha256 = "0cna4wavnhnb8j8vg119ap8mqkckx04z2gms2hsz4daywc51ghr8";
+    # This hash can be determined with:
+    #   nix hash file ${name}
+    hash = "sha256-MXjpDNUyAzMDj7CdOXLag+UOGU03AtAMjsrVtfXnV50=";
   };
 
   sourceRoot = "PragmataPro${version}";
