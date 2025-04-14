e65da26939bef3e43da4d36e0739a1fdb1a02015
Matt Moriarity <matt@mattmoriarity.com>
2025-04-13 21:41:01 -0600

    atticd: run in netns behind tls tunnels
    #117
    
    fixes #102

--- services/atticd/default.nix
+++ services/atticd/default.nix
@@ -20,7 +20,11 @@ in
     };
 
     ingress.virtualHosts.attic = {
-      upstream.service.name = "attic";
+      upstream = {
+        service.name = "attic";
+        tls.enable = true;
+      };
+
       enableAuthProxy = false;
       useIPv4Proxy = true;
     };
@@ -28,12 +32,14 @@ in
     services.atticd = {
       enable = true;
       settings = {
-        listen = "[::]:8100";
+        listen = "[::1]:8100";
         database.url = "postgresql:///atticd?host=/run/postgresql";
         storage = {
           type = "s3";
           region = "home";
           bucket = "attic-caches";
+          # when there's only a single chunk, then attic will redirect to a pre-signed URL for
+          # that chunk, so the endpoint needs to be publicly reachable
           endpoint = "https://garage.midna.dev";
         };
         compression.type = "zstd";
@@ -46,18 +52,48 @@ in
     vault-secrets.templates.attic-env.text = ''
       {{ with secret "kv/prod/services/atticd" }}
       ATTIC_SERVER_TOKEN_RS256_SECRET_BASE64={{ .Data.data.token_rs256_secret }}
-      AWS_ACCESS_KEY_ID={{ .Data.data.garage_key_id }}
-      AWS_SECRET_ACCESS_KEY={{ .Data.data.garage_secret_key }}
       {{ end }}
     '';
 
-    networking.firewall.allowedTCPPorts = [ 8100 ];
+    mjm.networkd.macvlan.enable = true;
+
+    systemd.services.atticd = {
+      bindsTo = [ "netns-bridge@attic.service" ];
+      after = [ "netns-bridge@attic.service" ];
+      serviceConfig.NetworkNamespacePath = "/run/netns/attic";
+      environment.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI = "/creds";
+    };
+
+    mjm.spire.tunnels = {
+      attic = {
+        mode = "server";
+        namespace = "attic";
+        port = 8100;
+        target = "localhost:8100";
+        allowIngress = true;
+        allowedServices = [ "consul-agent" ];
+      };
+      attic-s3-creds = {
+        mode = "client";
+        namespace = "attic";
+        listen = "169.254.170.2:80";
+        target = "spiffe-garage.service.consul:3899";
+        service = "spiffe-garage";
+      };
+      consul-attic = {
+        mode = "client";
+        socket = "/run/consul-checks/attic.sock";
+        target = "localhost:8100";
+        service = "attic";
+      };
+    };
 
     services.consul.services.attic = {
       port = 8100;
 
       checks.up = {
         http.path = "/";
+        http.socket = "/run/consul-checks/attic.sock";
       };
     };
 
