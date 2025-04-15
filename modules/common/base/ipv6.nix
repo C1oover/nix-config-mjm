059d54331b5f22b2b237b878580cae5ddd0516b8
Matt Moriarity <matt@slab.com>
2025-04-15 11:36:51 -0600

    update ipv6 prefix

--- modules/common/base/ipv6.nix
+++ modules/common/base/ipv6.nix
@@ -8,6 +8,6 @@ in
   };
 
   config = {
-    mjm.ipv6Prefix = "2601:282:0:13d8";
+    mjm.ipv6Prefix = "2601:282:0:1631";
   };
 }
--- packages/dippy/infra/dns_midna.go
+++ packages/dippy/infra/dns_midna.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	ipv6Prefix = "2601:282:0:13d8"
+	ipv6Prefix = "2601:282:0:1631"
 	aion       = "5.78.46.61"
 )
 
