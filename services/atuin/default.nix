d1f3fa183296d3ee08512a1a18667bcb30916ed7
Matt Moriarity <matt@mattmoriarity.com>
2025-04-18 21:59:43 -0600

    atuin: serve via tls tunnel
    fixes #103

--- services/atuin/default.nix
+++ services/atuin/default.nix
@@ -17,14 +17,43 @@ in
     mjm.services.atuin = { };
 
     ingress.virtualHosts.atuin = {
-      upstream.service.name = "atuin";
+      upstream = {
+        service.name = "atuin";
+        tls.enable = true;
+      };
+
       enableAuthProxy = false;
     };
 
     services.atuin = {
       enable = true;
-      host = "::";
-      openFirewall = true;
+      host = "::1";
+      port = 8888;
+    };
+
+    systemd.services.atuin = {
+      bindsTo = [ "netns-bridge@atuin.service" ];
+      after = [ "netns-bridge@atuin.service" ];
+      serviceConfig = {
+        NetworkNamespacePath = "/run/netns/atuin";
+      };
+    };
+
+    mjm.spire.tunnels = {
+      atuin = {
+        mode = "server";
+        namespace = "atuin";
+        inherit (config.services.atuin) port;
+        target = "localhost:8888";
+        allowIngress = true;
+        allowedServices = [ "consul-agent" ];
+      };
+      consul-atuin = {
+        mode = "client";
+        socket = "/run/consul-checks/atuin.sock";
+        target = "localhost:8888";
+        service = "atuin";
+      };
     };
 
     services.consul.services.atuin = {
@@ -32,6 +61,7 @@ in
 
       checks.up = {
         http.path = "/";
+        http.socket = "/run/consul-checks/atuin.sock";
       };
     };
 
