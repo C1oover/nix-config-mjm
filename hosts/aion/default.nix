231b139691962de477b1dee98af1e20900fc9e70
Matt Moriarity <matt@mattmoriarity.com>
2025-04-18 20:51:45 -0600

    aion: fix networkd config
    whoops, broke this when changing stuff for the homelab machines

--- hosts/aion/default.nix
+++ hosts/aion/default.nix
@@ -16,9 +16,9 @@
   networking.useDHCP = false;
   systemd.network = {
     enable = true;
-    networks."10-lan".enable = false;
+    networks."10-primary-lan".enable = false;
     networks."10-wan" = {
-      matchConfig.Name = "enp1s0";
+      matchConfig.Name = "lan0";
       networkConfig.DHCP = "ipv4";
       address = [ "2a01:4ff:1f0:879b::1/64" ];
       routes = [ { Gateway = "fe80::1"; } ];
