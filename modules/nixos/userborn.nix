3a0bf38a005974e7035fa5e0f6bffeddf5aaf028
Matt Moriarity <matt@mattmoriarity.com>
2025-05-03 15:08:36 -0600

    nixos/userborn: leave comment about mutable etc overlay

--- modules/nixos/userborn.nix
+++ modules/nixos/userborn.nix
@@ -12,6 +12,15 @@ in
 
   config = mkIf cfg.enable {
     system.etc.overlay.enable = true;
+
+    # known reasons this can't be done yet:
+    # - ssh keys get generated and placed in /etc
+    # - consul ipv6 address is generated at runtime and placed at /etc/consul-addrs.json
+    # these could probably be addressed at some point, by putting things in /run and
+    # leaving symlinks in /etc where needed.
+    #
+    # system.etc.overlay.mutable = !config.mjm.minimal.enable;
+
     services.userborn = {
       enable = true;
       passwordFilesLocation = "/var/lib/nixos";
