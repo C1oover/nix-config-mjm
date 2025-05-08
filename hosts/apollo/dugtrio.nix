88b86d538f3f53cb018cd098a089bfd57701cc87
Matt Moriarity <matt@mattmoriarity.com>
2025-05-07 18:48:31 -0600

    dugtrio: switch back to squashfs store disk

--- hosts/apollo/dugtrio.nix
+++ hosts/apollo/dugtrio.nix
@@ -9,6 +9,7 @@
 
       mjm.profiles.microvm = {
         enable = true;
+        useHostStore = false;
         macAddress = "02:CB:78:AD:51:4A";
         machineId = "91a269b24ff06ee998280e8e68159f6f";
       };
@@ -16,6 +17,7 @@
       # yikes, gitlab needs a lot of ram
       microvm.mem = 8192;
       microvm.vcpu = 4;
+      microvm.storeDiskType = "squashfs";
       system.stateVersion = "25.05";
     };
   };
