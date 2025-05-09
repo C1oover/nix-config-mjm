78ef22ad3073f9e9f2b36af1164caa03eef3c7eb
Matt Moriarity <matt@mattmoriarity.com>
2025-05-09 12:00:37 -0600

    slowbro: increase ram
    still getting OOM

--- hosts/apollo/slowbro.nix
+++ hosts/apollo/slowbro.nix
@@ -9,7 +9,7 @@
         machineId = "429061868868aadc419afc5b681d8513";
       };
       microvm.vcpu = 2;
-      microvm.mem = 1024;
+      microvm.mem = 1536;
       system.stateVersion = "25.05";
     };
   };
