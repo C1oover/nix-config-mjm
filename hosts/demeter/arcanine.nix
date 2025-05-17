6ebf67f0192a3f015afc2bff7e36d6a96c81d270
Matt Moriarity <matt@mattmoriarity.com>
2025-05-17 16:16:45 -0600

    arcanine: increase ram

--- hosts/demeter/arcanine.nix
+++ hosts/demeter/arcanine.nix
@@ -8,7 +8,7 @@
         macAddress = "02:8E:FB:AF:7D:0E";
         machineId = "41789ee482c9e489e5b252d36825fc6e";
       };
-      microvm.mem = 1536;
+      microvm.mem = 2048;
       system.stateVersion = "25.05";
     };
   };
