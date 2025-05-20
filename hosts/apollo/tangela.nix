a059273d084991691f0f7bfc4195d54441381f36
Matt Moriarity <matt@mattmoriarity.com>
2025-05-20 11:57:10 -0600

    tangela: increase ram
    otherwise the task that processes a document gets oom killed

--- hosts/apollo/tangela.nix
+++ hosts/apollo/tangela.nix
@@ -8,7 +8,7 @@
         macAddress = "02:F1:55:CA:CA:BC";
         machineId = "23cc22f5b42f9c54e945feee6823ef93";
       };
-      microvm.mem = 1536;
+      microvm.mem = 2048;
       system.stateVersion = "25.05";
     };
   };
