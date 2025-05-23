dcd43c4993c9dce3a166a32bd140e26a03648dde
Matt Moriarity <matt@mattmoriarity.com>
2025-05-22 20:20:59 -0600

    bump ram for lapras and staryu

--- hosts/apollo/lapras.nix
+++ hosts/apollo/lapras.nix
@@ -8,6 +8,7 @@
         macAddress = "02:9A:1C:47:DD:5A";
         machineId = "22d9a7315adb552e009fe6bc682bb9b4";
       };
+      microvm.mem = 768;
       system.stateVersion = "25.05";
     };
   };
--- hosts/artemis/staryu.nix
+++ hosts/artemis/staryu.nix
@@ -8,6 +8,7 @@
         macAddress = "02:99:26:6E:1E:69";
         machineId = "bffcffe64e7b94ef454140b86829e2e1";
       };
+      microvm.mem = 768;
       system.stateVersion = "25.05";
     };
   };
