3ceb505e5423689ab74af79747ff15f1cbf2a536
Matt Moriarity <matt@mattmoriarity.com>
2025-05-28 10:49:40 -0600

    jolteon: remove old join token

--- hosts/demeter/jolteon.nix
+++ hosts/demeter/jolteon.nix
@@ -3,8 +3,6 @@
     config = {
       mjm.lidarr.enable = true;
 
-      mjm.spire.agent.joinToken = "8add9319-c438-4ee2-bb44-12f8f2064306";
-
       mjm.profiles.microvm = {
         enable = true;
         macAddress = "02:DF:D2:A3:DE:CE";
