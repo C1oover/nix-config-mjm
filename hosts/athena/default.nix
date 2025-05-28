6e454274664ac67571bdd19cf2d28b248e7825f6
Matt Moriarity <matt@mattmoriarity.com>
2025-05-28 17:07:20 -0600

    athena: enable erofs

--- hosts/athena/default.nix
+++ hosts/athena/default.nix
@@ -25,11 +25,27 @@
   # without this, it's gonna use llvmpipe and the performance will be just total ass
   hardware.asahi.withRust = true;
 
+  boot.kernelPatches = [
+    {
+      name = "enable-erofs";
+      patch = null;
+      extraStructuredConfig = with lib.kernel; {
+        EROFS_FS = module;
+        EROFS_FS_XATTR = yes;
+        EROFS_FS_POSIX_ACL = yes;
+        EROFS_FS_SECURITY = yes;
+        EROFS_FS_BACKED_BY_FILE = yes;
+        EROFS_FS_ZIP = yes;
+        EROFS_FS_ZIP_LZMA = yes;
+        EROFS_FS_ZIP_DEFLATE = yes;
+        EROFS_FS_ZIP_ZSTD = yes;
+        EROFS_FS_ONDEMAND = yes;
+      };
+    }
+  ];
+
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
-
-  # erofs doesn't seem to be available here?
-  system.etc.overlay.enable = lib.mkForce false;
 
   # this is a little sketch, but for the moment it seems like the least bad way to let
   # this system build in CI. basically, if this firmware directory can't be found, it
